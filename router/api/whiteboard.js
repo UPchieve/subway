5422bf99fe62c91a36dabd2ac6a1e67c43ab2643
David Piccolella <davepiccolella@gmail.com>
2017-01-28 13:00:50 -0500

    Adding whiteboard socket events

--- router/api/whiteboard.js
+++ router/api/whiteboard.js
@@ -10,7 +10,73 @@ module.exports = function(app){
 
   io.on('connection', function(socket){
     console.log('a user connected');
-  });
+
+    socket.on('drawClick', function(data) {
+      socket.broadcast.emit('draw', {
+        x: data.x,
+        y: data.y,
+        type: data.type
+      });
+    });
+
+    socket.on('saveImage', function() {
+      socket.broadcast.emit('save');
+    });
+
+    socket.on('undoClick', function() {
+      socket.broadcast.emit('undo');
+    });
+
+    socket.on('clearClick', function() {
+      socket.broadcast.emit('clear');
+    });
+
+    socket.on('changeColor', function(data) {
+      socket.broadcast.emit('color', data);
+    });
+
+    socket.on('changeWidth', function(data) {
+      socket.broadcast.emit('width', data);
+    });
+
+    socket.on('dragStart', function(data) {
+    	socket.broadcast.emit('dstart', {
+    		x: data.x,
+	    	y: data.y,
+	    	color:data.color
+    	});
+    });
+
+    socket.on('dragAction', function(data) {
+    	socket.broadcast.emit('drag', {
+    		x: data.x,
+	    	y: data.y,
+	    	color:data.color
+    	});
+    });
+
+    socket.on('dragEnd', function(data) {
+    	socket.broadcast.emit('dend', {
+    		x: data.x,
+	    	y: data.y,
+	    	color:data.color
+    	});
+    });
+
+    socket.on('insertText', function(data) {
+      socket.broadcast.emit('text', {
+        text: data.text,
+        x: data.x,
+        y: data.y
+      });
+    });
+
+    socket.on('resetScreen', function() {
+      socket.broadcast.emit('reset');
+    });
+  });		
+
+
 
   var port = config.socketsPort;
   server.listen(port);
