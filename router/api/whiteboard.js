874196a757cb29ef212e194c31cfcfed2962f806
David Piccolella <davepiccolella@gmail.com>
2017-02-18 10:02:04 -0500

    Fixing room privacy

--- router/api/whiteboard.js
+++ router/api/whiteboard.js
@@ -12,6 +12,9 @@ module.exports = function(app){
     var room;
 
     socket.on('room', function(pRoom){
+      if (room) {
+        socket.leave(room);
+      }
       socket.join(pRoom);
       room = pRoom;
       console.log('Joining room', pRoom);
@@ -19,7 +22,7 @@ module.exports = function(app){
 
     socket.on('drawClick', function(data) {
       if (!room) return;
-      socket.to(room).emit('draw', {
+      io.to(room).emit('draw', {
         x: data.x,
         y: data.y,
         type: data.type
@@ -28,33 +31,33 @@ module.exports = function(app){
 
     socket.on('saveImage', function() {
       if (!room) return;
-      socket.to(room).emit('save');
+      io.to(room).emit('save');
     });
 
     socket.on('undoClick', function() {
       if (!room) return;
-      socket.to(room).emit('undo');
+      io.to(room).emit('undo');
     });
 
     socket.on('clearClick', function() {
       if (!room) return;
-      socket.to(room).emit('clear');
+      io.to(room).emit('clear');
     });
 
     socket.on('changeColor', function(data) {
       if (!room) return;
-      socket.to(room).emit('color', data);
+      io.to(room).emit('color', data);
     });
 
     socket.on('changeWidth', function(data) {
       if (!room) return;
-      socket.to(room).emit('width', data);
+      io.to(room).emit('width', data);
     });
 
     socket.on('dragStart', function(data) {
       if (!room) return;
       console.log('Emitting to room', room);
-      socket.to(room).emit('dstart', {
+      io.to(room).emit('dstart', {
         x: data.x,
         y: data.y,
         color:data.color
@@ -63,7 +66,7 @@ module.exports = function(app){
 
     socket.on('dragAction', function(data) {
       if (!room) return;
-      socket.to(room).emit('drag', {
+      io.to(room).emit('drag', {
         x: data.x,
         y: data.y,
         color:data.color
@@ -72,7 +75,7 @@ module.exports = function(app){
 
     socket.on('dragEnd', function(data) {
       if (!room) return;
-      socket.to(room).emit('dend', {
+      io.to(room).emit('dend', {
         x: data.x,
         y: data.y,
         color:data.color
@@ -81,7 +84,7 @@ module.exports = function(app){
 
     socket.on('insertText', function(data) {
       if (!room) return;
-      socket.to(room).emit('text', {
+      io.to(room).emit('text', {
         text: data.text,
         x: data.x,
         y: data.y
@@ -90,7 +93,7 @@ module.exports = function(app){
 
     socket.on('resetScreen', function() {
       if (!room) return;
-      socket.to(room).emit('reset');
+      io.to(room).emit('reset');
     });
   });
 
