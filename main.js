b298a30d7164ace4e334c3720c4cdf4b076d4b3d
Corey <corey@x64.co>
2017-03-03 00:07:32 -0800

    Serve static app assets in /dist

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(config.sessionSecret));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(busboy());
 app.use(cors({
 	origin: true,
--- router/index.js
+++ router/index.js
@@ -5,4 +5,19 @@ module.exports = function(app){
 
   require('./auth')(app);
   require('./api')(app);
+
+  // Determine if incoming request is a static asset
+  var isStaticReq = function(req){
+    return ['/auth', '/api', '/js', '/css'].some(function(whitelist){
+      return req.url.substr(0, whitelist.length) === whitelist;
+    });
+  };
+
+  // Single page app routing
+  app.use(function(req, res, next){
+    if (isStaticReq(req)){
+      return next();
+    }
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
 };
