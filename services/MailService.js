ffcd2c435feef88555c9bdbd702afa095e4df086
Corey <corey@x64.co>
2017-03-08 12:36:53 -0800

    Switching from nodemailer/gmail to sendgrid

--- services/MailService.js
+++ services/MailService.js
@@ -1,25 +1,53 @@
 var config = require('../config');
 
-var transporter = require('nodemailer').createTransport(config.mail.smtpConfig);
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var helper = require('sendgrid').mail;
 
 module.exports = {
+  sendEmail: function(options, callback){
+    options = options || {};
+
+    var fromEmail = new helper.Email(options.from),
+        toEmail = new helper.Email(options.to),
+        subject = options.subject || 'New message from UPchieve',
+        content = new helper.Content('text/plain', options.content);
+
+    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(err, res) {
+      if (err) {
+        console.log('SendGrid error');
+        console.log(err);
+      } else {
+        console.log('Sent email via SendGrid:', res.statusCode);
+        console.log(res.body);
+        console.log(res.headers);
+      }
+      callback(err, res);
+    });
+  },
   sendVerification: function(options, callback){
     var email = options.email,
         token = options.token;
 
     var url = 'http://' + config.client.host + '/#/action/verify/' + token;
 
-    var mailOptions = {
+    this.sendEmail({
       to: email,
-      from: config.mail.senders.noreply,
+      from: 'noreply@upchieve.org',
       subject: 'Welcome to UPchieve! Please verify your account',
-      text: [
+      content: [
         'Hello!',
         'Thank you registering an account on UPchieve. In order to use the website, follow the link below to verify your account:',
         url
       ].join('\n\n')
-    };
+    }, callback);
 
-    transporter.sendMail(mailOptions, callback);
   }
 };
