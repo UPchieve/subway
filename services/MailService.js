b54c43c79e77b84a48c2d6f5d78585c7b9b2a517
zuoyuanh <david.hzy97@gmail.com>
2017-06-04 22:05:39 -0700

    Add files via upload

--- services/MailService.js
+++ services/MailService.js
@@ -1,49 +1,79 @@
 var config = require('../config');
 
-var sg = require('sendgrid')(config.sendgridApiKey);
-var helper = require('sendgrid').mail;
+var transporter = require('nodemailer').createTransport(config.mail.smtpConfig);
 
 module.exports = {
-  sendEmail: function(options, callback){
-    options = options || {};
-
-    var fromEmail = new helper.Email(options.from),
-        toEmail = new helper.Email(options.to),
-        subject = options.subject || 'New message from UPchieve',
-        content = new helper.Content('text/plain', options.content);
-
-    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
-
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON()
-    });
-
-    sg.API(request, function(err, res) {
-      if (err) {
-        console.log('SendGrid error');
-        console.log(err);
-      }
-      callback(err, res);
-    });
-  },
   sendVerification: function(options, callback){
+
     var email = options.email,
         token = options.token;
 
     var url = 'http://' + config.client.host + '/#/action/verify/' + token;
 
-    this.sendEmail({
+    /* var mailOptions = {
       to: email,
-      from: 'noreply@upchieve.org',
+      from: config.mail.senders.noreply,
       subject: 'Welcome to UPchieve! Please verify your account',
-      content: [
+      text: [
         'Hello!',
         'Thank you registering an account on UPchieve. In order to use the website, follow the link below to verify your account:',
         url
       ].join('\n\n')
-    }, callback);
+    }; 
+
+    transporter.sendMail(mailOptions, callback);
+
+    */
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: {
+        personalizations: [
+          {
+            to: [
+              {
+                email: email
+              }
+            ],
+            substitutions: {
+                "-userEmail-": email, 
+                "-verifyLink-": url
+            }
+          }
+        ],
+        from: {
+          email: 'upchievetest@hotmail.com'
+        },
+        template_id: '142a621c-127a-46a1-b36a-d0689fd07877'
+      }
+    });
+
+    // With promise
+    // sg.API(request)
+      // .then(function (response) {
+        // console.log(response.statusCode);
+        // console.log(response.body);
+        // console.log(response.headers);
+      //  })
+      // .catch(function (error) {
+        // error is an instance of SendGridError
+        // The full response is attached to error.response
+        // console.log(error.response.statusCode);
+      // });
+
+    // With callback
+    sg.API(request, function (error, response) {
+      if (error) {
+        console.log('Error response received');
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+      } else {
+        console.log('Mail sent')
+      }
+    });
 
   }
 };
