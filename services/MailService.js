8499e12fa55943419e2718b22ad55f8eb96db8af
zuoyuanh <david.hzy97@gmail.com>
2017-06-16 13:11:09 -0700

    Add files via upload

--- services/MailService.js
+++ services/MailService.js
@@ -1,8 +1,20 @@
 var config = require('../config');
+var helper = require('sendgrid').mail
+var sendgrid = require('sendgrid')(config.sendgrid.apiKey);
 
 // var transporter = require('nodemailer').createTransport(config.mail.smtpConfig);
 
 module.exports = {
+
+  sendTemplatedEmail: function(mail, callback) {
+  	  var request = sendgrid.emptyRequest({
+	  method: 'POST',
+	  path: '/v3/mail/send',
+	  body: mail.toJSON()
+	});
+
+	sendgrid.API(request, callback);
+  }, 
 
   sendVerification: function(options, callback){
 
@@ -11,17 +23,16 @@ module.exports = {
 
     var url = 'http://' + config.client.host + '/#/action/verify/' + token;
 
-    var helper = require('sendgrid').mail
-    var from_email = new helper.Email(config.mail.senders.noreply);
-    var to_email = new helper.Email(email);
-    var subject = '';
-    var content = new helper.Content('text/html', '');
-    var mail = new helper.Mail(from_email, subject, to_email, content);
-    mail.personalizations[0].addSubstitution(new helper.Substitution('-userEmail-', email));
-    mail.personalizations[0].addSubstitution(new helper.Substitution('-verifyLink-', url));
-    mail.setTemplateId('142a621c-127a-46a1-b36a-d0689fd07877');
+	var from_email = new helper.Email(config.mail.senders.noreply);
+	var to_email = new helper.Email(email);
+    var subject = 'I\'m replacing the subject tag';
+	var content = new helper.Content('text/html', 'I\'m replacing the <strong>body tag</strong>');
+	var mail = new helper.Mail(from_email, subject, to_email, content);
+	mail.personalizations[0].addSubstitution(new helper.Substitution('-userEmail-', email));
+	mail.personalizations[0].addSubstitution(new helper.Substitution('-verifyLink-', url));
+	mail.setTemplateId(config.sendgrid.templateId);
 
-    require('./TemplatedEmail').sendTemplatedEmail(mail, callback);
+	this.sendTemplatedEmail(mail, callback);
 
   }
 
