6f6e3e7637d7a5538eb5dceb5bad1e32b511157a
ilakumar <ilakkumar@gmail.com>
2017-06-19 14:39:06 -0400

    move endpoints to index.js

--- router/auth/index.js
+++ router/auth/index.js
@@ -5,6 +5,7 @@ var passport = require('passport');
 var MongoStore = require('connect-mongo')(session);
 
 var VerificationCtrl = require('../../controllers/VerificationCtrl');
+var ResetPasswordCtrl = require('../../controllers/ResetPasswordCtrl');
 
 var config = require('../../config.js');
 var User = require('../../models/User.js');
@@ -185,6 +186,39 @@ module.exports = function(app){
       }
     });
   });
+
+  router.post('/reset/send', function(req, res){
+		var userId = req.user && req.user._id;
+
+		if (!userId){
+			return res.json({err: 'Must be authenticated to send password reset email'});
+		}
+
+		ResetPasswordCtrl.initiateReset({
+			userId: userId
+		}, function(err, email){
+			if (err){
+				res.json({err: err});
+			} else {
+				res.json({msg: 'Reset email sent to ' + email});
+			}
+		});
+	});
+
+	router.post('/reset/confirm', function(req, res){
+		var token = req.body.token;
+		ResetPasswordCtrl.finishReset({
+			token: token
+		}, function(err, user){
+			if (err){
+				res.json({err: err});
+			} else {
+				res.json({
+					msg: 'Password reset successful'
+				});
+			}
+		});
+	});
 
   app.use('/auth', router);
 };
