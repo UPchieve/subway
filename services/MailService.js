cd1a1a6ccbe68d298f1e587bcb8fd66c4381f994
zuoyuanh <david.hzy97@gmail.com>
2017-06-22 16:41:27 -0700

    Add files via upload

--- services/MailService.js
+++ services/MailService.js
@@ -6,6 +6,8 @@ var sendgrid = require('sendgrid')(config.sendgrid.apiKey);
 
 module.exports = {
 
+  // HTML version
+
   sendTemplatedEmail: function(mail, callback) {
   	  var request = sendgrid.emptyRequest({
 	  method: 'POST',
@@ -33,6 +35,51 @@ module.exports = {
 	mail.setTemplateId(config.sendgrid.templateId);
 
 	this.sendTemplatedEmail(mail, callback);
+
+  },
+
+  // plain text version
+
+  sendEmail: function(options, callback){
+    options = options || {};
+
+    var fromEmail = new helper.Email(options.from),
+        toEmail = new helper.Email(options.to),
+        subject = options.subject || 'New message from UPchieve',
+        content = new helper.Content('text/plain', options.content);
+
+    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(err, res) {
+      if (err) {
+        console.log('SendGrid error');
+        console.log(err);
+      }
+      callback(err, res);
+    });
+  },
+  sendPlainTextVerification: function(options, callback){
+    var email = options.email,
+        token = options.token;
+
+    var url = 'http://' + config.client.host + '/#/action/verify/' + token;
+
+    this.sendEmail({
+      to: email,
+      from: 'noreply@upchieve.org',
+      subject: 'Welcome to UPchieve! Please verify your account',
+      content: [
+        'Hello!',
+        'Thank you registering an account on UPchieve. In order to use the website, follow the link below to verify your account:',
+        url
+      ].join('\n\n')
+    }, callback);
 
   }
 
