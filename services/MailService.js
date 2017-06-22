4db31b8eadb3e04404f26eee53e5b52b9b6c95e9
zuoyuanh <david.hzy97@gmail.com>
2017-06-22 16:53:50 -0700

    Add files via upload

--- services/MailService.js
+++ services/MailService.js
@@ -9,7 +9,7 @@ module.exports = {
   // HTML version
 
   sendTemplatedEmail: function(mail, callback) {
-  	  var request = sendgrid.emptyRequest({
+  	var request = sendgrid.emptyRequest({
 	  method: 'POST',
 	  path: '/v3/mail/send',
 	  body: mail.toJSON()
@@ -27,8 +27,6 @@ module.exports = {
 
 	var from_email = new helper.Email(config.mail.senders.noreply);
 	var to_email = new helper.Email(email);
-    var subject = 'I\'m replacing the subject tag';
-	var content = new helper.Content('text/html', 'I\'m replacing the <strong>body tag</strong>');
 	var mail = new helper.Mail(from_email, subject, to_email, content);
 	mail.personalizations[0].addSubstitution(new helper.Substitution('-userEmail-', email));
 	mail.personalizations[0].addSubstitution(new helper.Substitution('-verifyLink-', url));
@@ -40,7 +38,7 @@ module.exports = {
 
   // plain text version
 
-  sendEmail: function(options, callback){
+  sendPlainTextEmail: function(options, callback){
     options = options || {};
 
     var fromEmail = new helper.Email(options.from),
@@ -63,24 +61,6 @@ module.exports = {
       }
       callback(err, res);
     });
-  },
-  sendPlainTextVerification: function(options, callback){
-    var email = options.email,
-        token = options.token;
-
-    var url = 'http://' + config.client.host + '/#/action/verify/' + token;
-
-    this.sendEmail({
-      to: email,
-      from: 'noreply@upchieve.org',
-      subject: 'Welcome to UPchieve! Please verify your account',
-      content: [
-        'Hello!',
-        'Thank you registering an account on UPchieve. In order to use the website, follow the link below to verify your account:',
-        url
-      ].join('\n\n')
-    }, callback);
-
   }
 
 };
