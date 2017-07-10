8f3fe466f87a532c23938f6c35abfdf9492ff2bf
ilakumar <ilakkumar@gmail.com>
2017-07-10 13:30:16 -0400

    fixing merge conflicts

--- services/MailService.js
+++ services/MailService.js
@@ -1,45 +1,117 @@
 var config = require('../config');
+var helper = require('sendgrid').mail
+var sendgrid = require('sendgrid')(config.sendgrid.apiKey);
 
-var transporter = require('nodemailer').createTransport(config.mail.smtpConfig);
+// var transporter = require('nodemailer').createTransport(config.mail.smtpConfig);
 
 module.exports = {
+
+  // HTML version
+
+  sendTemplatedEmail: function(mail, callback) {
+  	var request = sendgrid.emptyRequest({
+  	  method: 'POST',
+  	  path: '/v3/mail/send',
+  	  body: mail.toJSON()
+  	});
+
+    sendgrid.API(request, callback);
+  },
+
   sendVerification: function(options, callback){
+
     var email = options.email,
     token = options.token;
 
     var url = 'http://' + config.client.host + '/#/action/verify/' + token;
 
-    var mailOptions = {
-      to: email,
-      from: config.mail.senders.noreply,
-      subject: 'Welcome to UPchieve! Please verify your account',
-      text: [
-        'Hello!',
-        'Thank you registering an account on UPchieve. In order to use the website, follow the link below to verify your account:',
-        url
-      ].join('\n\n')
-    };
+    var fromEmail = new helper.Email(config.mail.senders.noreply),
+        toEmail = new helper.Email(email),
+        subject = '[UPchieve] Verify your email address';
+        content = new helper.Content('text/plain', options.content);
 
-    transporter.sendMail(mailOptions, callback);
+	var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+	mail.personalizations[0].addSubstitution(new helper.Substitution('-userEmail-', email));
+	mail.personalizations[0].addSubstitution(new helper.Substitution('-verifyLink-', url));
+	mail.setTemplateId(config.sendgrid.templateId);
+
+	this.sendTemplatedEmail(mail, callback);
+
+  },
+
+  // plain text version
+
+  sendPlainTextEmail: function(options, callback){
+    options = options || {};
+
+    var fromEmail = new helper.Email(options.from),
+        toEmail = new helper.Email(options.to),
+        subject = options.subject || 'New message from UPchieve',
+        content = new helper.Content('text/plain', options.content);
+
+    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(err, res) {
+      if (err) {
+        console.log('SendGrid error');
+        console.log(err);
+      }
+      callback(err, res);
+    });
+  },
+
+  // plain text version
+
+  sendReset: function(options, callback){
+    options = options || {};
+
+    var fromEmail = new helper.Email(options.from),
+        toEmail = new helper.Email(options.to),
+        subject = options.subject || 'New message from UPchieve',
+        content = new helper.Content('text/plain', options.content);
+
+    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(err, res) {
+      if (err) {
+        console.log('SendGrid error');
+        console.log(err);
+      }
+      callback(err, res);
+    });
   },
 
   sendReset: function(options, callback){
     var email = options.email,
         token = options.token;
 
-    var url = 'http://' + config.client.host + '/#/setpassword/' + token;
+    var url = 'http://' + config.client.host + '/#/action/reset/' + token;
 
-    var mailOptions = {
-      to: email,
-      from: config.mail.senders.noreply,
-      subject: 'Did you want to reset your password?',
-      text: [
-        'Click on this link to choose a new password!', url,
-        'If you received this email by accident, you can just ignore it and your password will not change.'
-      ].join('\n\n')
-    };
+    var fromEmail = new helper.Email(config.mail.senders.noreply),
+        toEmail = new helper.Email(email),
+        subject = '[UPchieve] Did you want to reset your password?',
+        content = [
+          'Click on this link to choose a new password!', url,
+          'If you received this email by accident, you can just ignore it and your password will not change.',
+        ].join('\n\n');
 
-    transporter.sendMail(mailOptions, callback);
+    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+    mail.personalizations[0].addSubstitution(new helper.Substitution('-userEmail-', email));
+    mail.personalizations[0].addSubstitution(new helper.Substitution('-verifyLink-', url));
+    mail.setTemplateId(config.sendgrid.templateId);
+
+    this.sendTemplatedEmail(mail, callback);
   }
-
 };
