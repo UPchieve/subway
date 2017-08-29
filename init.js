dd09b0deaad65678174b75c36dbeccfb6112e870
ilakumar <ilakkumar@gmail.com>
2017-08-29 11:39:12 -0400

    add geometry quiz

--- init.js
+++ init.js
@@ -10,19 +10,23 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
 	console.log('Connected to database');
   var collection = db.collection('question');
-	try {
-		var json = require('./primer-dataset.json');
-	} catch (e) {
-		console.log(e);
+	var jsonArr = ['./geometry.json', './algebra.json'];
+	for (var i = 0; i < jsonArr.length; i++) {
+		try {
+			var json = require(jsonArr[i]);
+			console.log(json);
+		} catch (e) {
+			console.log(e);
+		}
+		collection.insertMany(json, function(err,result) {
+	    console.log(json);
+	    if (err) {
+	      throw new Error(err);
+	    }
+	    else {
+	      console.log('Successfully imported data');
+	      process.exit();
+	    }
+	  });
 	}
-  collection.insertMany(json, function(err,result) {
-    console.log(json);
-    if (err) {
-      throw new Error(err);
-    }
-    else {
-      console.log('Successfully imported data');
-      process.exit();
-    }
-  });
 });
--- models/Question.js
+++ models/Question.js
@@ -21,7 +21,8 @@ questionSchema.methods.parseQuestion = function(){
 
 questionSchema.statics.getSubcategories = function(category){
   var categoryToSubcategoryMap = {
-    algebra: ['linear equations', 'rational exponents and radicals', 'application of linear equations', 'two variable equations', 'rational expressions', 'complex numbers']
+    algebra: ['linear equations', 'rational exponents and radicals', 'application of linear equations', 'two variable equations', 'rational expressions', 'complex numbers'],
+    geometry: ['congruence and similarity', 'vertices', 'angles', 'circles', 'triangles', 'rectangles']
   };
   var subcategories = categoryToSubcategoryMap[category];
   return subcategories;
