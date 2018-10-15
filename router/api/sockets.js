b960a559f52d059e8b34a1fbfa04a27806ec3278
zuoyuanh <david@collegemate.me>
2018-10-14 18:54:10 -0700

    fix error while saving message

--- router/api/sockets.js
+++ router/api/sockets.js
@@ -1,63 +1,66 @@
-var http = require('http');
-var socket = require('socket.io');
+var http = require("http");
+var socket = require("socket.io");
 
-var config = require('../../config.js');
+var config = require("../../config.js");
 
-var User = require('../../models/User.js');
-var SessionCtrl = require('../../controllers/SessionCtrl.js');
+var User = require("../../models/User.js");
+var SessionCtrl = require("../../controllers/SessionCtrl.js");
 
-
-module.exports = function(app){
+module.exports = function(app) {
   var server = http.createServer(app);
   var io = socket(server);
 
-  io.on('connection', function(socket){
-
+  io.on("connection", function(socket) {
     // Session management
-    socket.on('join', function(data){
+    socket.on("join", function(data) {
       if (!data || !data.sessionId) return;
-      console.log('Joining session...', data.sessionId);
-      SessionCtrl.joinSession({
-        sessionId: data.sessionId,
-        user: data.user,
-        socket: socket
-      }, function(err, session){
-        if (err){
-          console.log('Could not join session');
-          io.emit('error', err);
-        } else {
-          socket.join(data.sessionId);
-          console.log('Session joined:', session._id);
-          console.log(session);
-          io.emit('sessions', SessionCtrl.getSocketSessions());
-          io.to(session._id).emit('session-change', session);
+      console.log("Joining session...", data.sessionId);
+      SessionCtrl.joinSession(
+        {
+          sessionId: data.sessionId,
+          user: data.user,
+          socket: socket
+        },
+        function(err, session) {
+          if (err) {
+            console.log("Could not join session");
+            io.emit("error", err);
+          } else {
+            socket.join(data.sessionId);
+            console.log("Session joined:", session._id);
+            console.log(session);
+            io.emit("sessions", SessionCtrl.getSocketSessions());
+            io.to(session._id).emit("session-change", session);
+          }
         }
-      })
+      );
     });
 
-    socket.on('disconnect', function(){
-      console.log('Client disconnected');
+    socket.on("disconnect", function() {
+      console.log("Client disconnected");
 
-      SessionCtrl.leaveSession({
-        socket: socket
-      }, function(err, session){
-        if (err){
-          console.log('Error leaving session', err);
-        } else if (session){
-          console.log('Left session', session._id)
-          socket.leave(session._id);
-          io.to(session._id).emit('session-change', session);
-          io.emit('sessions', SessionCtrl.getSocketSessions());
+      SessionCtrl.leaveSession(
+        {
+          socket: socket
+        },
+        function(err, session) {
+          if (err) {
+            console.log("Error leaving session", err);
+          } else if (session) {
+            console.log("Left session", session._id);
+            socket.leave(session._id);
+            io.to(session._id).emit("session-change", session);
+            io.emit("sessions", SessionCtrl.getSocketSessions());
+          }
         }
-      });
+      );
     });
 
-    socket.on('list', function(){
-      io.emit('sessions', SessionCtrl.getSocketSessions());
+    socket.on("list", function() {
+      io.emit("sessions", SessionCtrl.getSocketSessions());
     });
 
-
-    socket.on('message', function(data) {
+    socket.on("message", function(data) {
       if (!data.sessionId) return;
 
       var message = {
@@ -65,111 +68,112 @@ module.exports = function(app){
         contents: data.message
       };
 
-      SessionCtrl.get({
-        sessionId: data.sessionId
-      }, function(err, session){
-        session.saveMessage(message, function(err, savedMessage){
-          io.to(data.sessionId).emit('messageSend', {
-            contents: savedMessage.contents,
-            name: data.user.firstname,
-            picture: data.user.picture,
-            time: savedMessage.createdAt
+      SessionCtrl.get(
+        {
+          sessionId: data.sessionId
+        },
+        function(err, session) {
+          session.saveMessage(message, function(err, savedMessage) {
+            io.to(data.sessionId).emit("messageSend", {
+              contents: savedMessage.contents,
+              name: data.user.firstname,
+              picture: data.user.picture,
+              time: savedMessage.createdAt
+            });
           });
-        })
-      });
+        }
+      );
     });
 
     // Whiteboard interaction
 
-    socket.on('drawClick', function(data) {
+    socket.on("drawClick", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('draw', {
+      socket.broadcast.to(data.sessionId).emit("draw", {
         x: data.x,
         y: data.y,
         type: data.type
       });
     });
 
-    socket.on('saveImage', function(data) {
+    socket.on("saveImage", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('save');
+      socket.broadcast.to(data.sessionId).emit("save");
     });
 
-    socket.on('undoClick', function(data) {
+    socket.on("undoClick", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('undo');
+      socket.broadcast.to(data.sessionId).emit("undo");
     });
 
-    socket.on('clearClick', function(data) {
+    socket.on("clearClick", function(data) {
       if (!data || !data.sessionId) return;
-      io.to(data.sessionId).emit('clear');
+      io.to(data.sessionId).emit("clear");
     });
 
-    socket.on('drawing', function(data) {
+    socket.on("drawing", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('draw');
+      socket.broadcast.to(data.sessionId).emit("draw");
     });
 
-    socket.on('end', function(data) {
+    socket.on("end", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('end');
+      socket.broadcast.to(data.sessionId).emit("end");
     });
 
-    socket.on('changeColor', function(data) {
+    socket.on("changeColor", function(data) {
       if (!data || !data.sessionId) return;
-        socket.broadcast.to(data.sessionId).emit('color', data.color);
+      socket.broadcast.to(data.sessionId).emit("color", data.color);
     });
 
-    socket.on('changeWidth', function(data) {
+    socket.on("changeWidth", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('width', data.width);
+      socket.broadcast.to(data.sessionId).emit("width", data.width);
     });
 
-    socket.on('dragStart', function(data) {
+    socket.on("dragStart", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('dstart', {
+      socket.broadcast.to(data.sessionId).emit("dstart", {
         x: data.x,
         y: data.y,
-        color:data.color
+        color: data.color
       });
     });
 
-    socket.on('dragAction', function(data) {
+    socket.on("dragAction", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('drag', {
+      socket.broadcast.to(data.sessionId).emit("drag", {
         x: data.x,
         y: data.y,
-        color:data.color
+        color: data.color
       });
     });
 
-    socket.on('dragEnd', function(data) {
+    socket.on("dragEnd", function(data) {
       if (!data || !data.sessionId) return;
-      socket.broadcast.to(data.sessionId).emit('dend', {
+      socket.broadcast.to(data.sessionId).emit("dend", {
         x: data.x,
         y: data.y,
-        color:data.color
+        color: data.color
       });
     });
 
-    socket.on('insertText', function(data) {
+    socket.on("insertText", function(data) {
       if (!data || !data.sessionId) return;
-      io.to(data.sessionId).emit('text', {
+      io.to(data.sessionId).emit("text", {
         text: data.text,
         x: data.x,
         y: data.y
       });
     });
 
-    socket.on('resetScreen', function() {
-      io.to(data.sessionId).emit('reset');
+    socket.on("resetScreen", function() {
+      io.to(data.sessionId).emit("reset");
     });
   });
-
-
 
   var port = config.socketsPort;
   server.listen(port);
 
-  console.log('Sockets.io listening on port ' + port);
+  console.log("Sockets.io listening on port " + port);
 };
