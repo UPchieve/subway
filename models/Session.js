UPchieve/subway

commit 15f8d188d399ff847ab94ada5908b0ea1460e395

Persist the volunteer join time when a volunteer joins a session (#78)

models/Session.js
@@ -47,6 +47,10 @@ var sessionSchema = new mongoose.Schema({
 
   endedAt: {
     type: Date
+  },
+
+  volunteerJoinedAt: {
+    created: { type: Date }
   }
 
   // Scheduled sessions
@@ -88,6 +92,7 @@ sessionSchema.methods.saveWhiteboardUrl = function (whiteboardUrl, cb) {
 sessionSchema.methods.joinUser = function (user, cb) {
   if (user.isVolunteer) {
     this.volunteer = user
+    this.volunteerJoinedAt = new Date()
   } else {
     this.student = user
   }

controllers/SessionCtrl.js
@@ -8,7 +8,8 @@ var SocketSession = function (options) {
   this.sockets = {} // userId => socket
 }
 
-// Add a socket and user to the session. If the user already has a socket, disconnect and replace it
+// Add a socket and user to the session. If the user already has a socket,
+// disconnect and replace it
 SocketSession.prototype.join = function (options) {
   var user = options.user
   var socket = options.socket

commit 8268503ff215d8e259e128f5fe3318ad21c78649

Fix Session object volunteer join timestamp (#80)
Fixes a regression introduced in #78, caught on dev

models/Session.js
@@ -50,7 +50,7 @@ var sessionSchema = new mongoose.Schema({
   },
 
   volunteerJoinedAt: {
-    created: { type: Date }
+    type: Date
   }
 
   // Scheduled sessions