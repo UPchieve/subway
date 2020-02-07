cffc4c9287eac3e439b155c9abbafc88eacbdf6f
dmnisson <43731303+dmnisson@users.noreply.github.com>
2020-02-07 11:21:35 -0800

    Faster validation for calendar API (#217)
    * faster validation before setting availability object
    
    * lint
    
    Resolves #215

--- controllers/CalendarCtrl.js
+++ controllers/CalendarCtrl.js
@@ -3,7 +3,31 @@ module.exports = {
     const user = options.user
     const availability = options.availability
 
-    user.availability.set(availability)
+    // verify that availability object is defined and not null
+    if (!availability) {
+      // early exit
+      return callback(new Error('No availability object specified'))
+    }
+
+    // verify that all of the day-of-week and time-of-day properties are defined on the
+    // new availability object
+    if (
+      Object.keys(user.availability.toObject()).some(key => {
+        if (typeof availability[key] === 'undefined') {
+          // day-of-week property needs to be defined
+          return true
+        }
+
+        // time-of-day properties also need to be defined
+        return Object.keys(user.availability[key].toObject()).some(
+          key2 => typeof availability[key][key2] === 'undefined'
+        )
+      })
+    ) {
+      return callback(new Error('Availability object missing required keys'))
+    }
+
+    user.availability = availability
     user.availabilityLastModifiedAt = new Date().toISOString()
     user.save(function(err, user) {
       if (err) {
