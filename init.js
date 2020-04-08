097c34e94fc80dbb7598731cacf77850e3e69c3f
Tyler Cummings-Bond <ti2ger92@users.noreply.github.com>
2020-04-08 18:36:21 -0400

    Test schools zips (#274)
    * Refactor and add test schools
    
    * add seed zipCodes
    
    * update package-lock.json
    
    * seed zip code median income as number instead of string
    
    * misc cleanup
    
    Resolves #265 and #266

--- init.js
+++ init.js
@@ -1,36 +1,80 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const ejson = require('mongodb-extended-json')
 
 // Configuration
-var config = require('./config')
+const config = require('./config')
 
 // Database
 mongoose.connect(config.database, { useNewUrlParser: true })
-var db = mongoose.connection
+const db = mongoose.connection
+
 db.on('error', console.error.bind(console, 'connection error:'))
+
 db.once('open', function() {
   console.log('Connected to database')
-  var collection = db.collection('question')
-  var jsonArr = [
-    'geometry',
-    'algebra',
-    'trigonometry',
-    'precalculus',
-    'calculus',
-    'planning',
-    'essays',
-    'applications'
-  ]
-  let promises = []
-  for (var i = 0; i < jsonArr.length; i++) {
-    try {
-      var json = require('./seeds/questions/' + jsonArr[i] + '.json')
-      console.log(json)
-    } catch (e) {
-      console.log(e)
-    }
 
-    promises.push(collection.insertMany(json))
-  }
+  const promises = []
+
+  // Data about the seed data we intend to import / update from this file
+  const seedDataMetadata = [
+    {
+      folder: 'schools/',
+      collection: 'schools',
+      idField: 'upchieveId',
+      files: ['test_high_schools']
+    },
+    {
+      folder: 'zipcodes/',
+      collection: 'zipcodes',
+      idField: 'zipCode',
+      files: ['test_zipcodes']
+    },
+    {
+      folder: 'questions/',
+      collection: 'question',
+      idField: 'questionText',
+      files: [
+        'geometry',
+        'algebra',
+        'trigonometry',
+        'precalculus',
+        'calculus',
+        'planning',
+        'essays',
+        'applications'
+      ]
+    }
+  ]
+
+  // For each of the above metadata items, replace each record in each file with the value from seed data
+  seedDataMetadata.forEach(seedDataMetadataItem => {
+    const aCollection = db.collection(seedDataMetadataItem.collection)
+
+    seedDataMetadataItem.files.forEach(file => {
+      const seedData = require('./seeds/' +
+        seedDataMetadataItem.folder +
+        file +
+        '.json')
+
+      // use Extended JSON to handle formats like "$date" in json files
+      const deserializedSeedData = ejson.deserialize(seedData)
+
+      deserializedSeedData.forEach(record => {
+        // Build a Unique ID Key for each record to be updated. Start with empty object
+        const idKey = {}
+
+        // Add a single key/value: key is seedDataMetadataItem.idField
+        idKey[seedDataMetadataItem.idField] =
+          record[seedDataMetadataItem.idField]
+
+        const replacePromise = aCollection.replaceOne(idKey, record, {
+          upsert: true
+        })
+
+        promises.push(replacePromise)
+      })
+    })
+  })
 
   Promise.all(promises)
     .then(() => {
