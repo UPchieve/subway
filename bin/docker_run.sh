bcc22c94f441f7bc9801f55770201c615def2baa
Tyler Cummings-Bond <ti2ger92@users.noreply.github.com>
2020-05-28 15:01:13 -0400

    add redis to docker-compose (#369)

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -6,6 +6,12 @@
 #     database: 'mongodb://db:27017/upchieve',
 sed -i 's/mongodb:\/\/localhost/mongodb:\/\/db/g' ../config.ts
 
+# Alter a line in default config.ts:
+#     redisConnectionString: 'redis://127.0.0.1:6379'
+# New line should read
+#     redisConnectionString: 'redis://cache:6379'
+sed -i 's/redis:\/\/127\.0\.0\.1/redis:\/\/cache/g' ../config.ts
+
 sleep 15
 
 npx ts-node init
