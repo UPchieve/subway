d55b3eab1c55ac4dde8bdf444c9984a5063c4a06
Jack Keating <jack.keating@upchieve.org>
2020-09-23 15:39:12 -0500

    Toggle deactivated (#464)
    * allow editing isDeactivated
    
    * update sendgrid contact if isDeactivated changes

--- router/api/user.js
+++ router/api/user.js
@@ -1,5 +1,6 @@
 const UserCtrl = require('../../controllers/UserCtrl')
 const UserService = require('../../services/UserService')
+const MailService = require('../../services/MailService')
 const AwsService = require('../../services/AwsService')
 const User = require('../../models/User')
 const Volunteer = require('../../models/Volunteer')
@@ -36,10 +37,15 @@ module.exports = function(router) {
   // @note: Currently, only volunteers are able to update their profile
   router.put('/user', async (req, res, next) => {
     const { _id } = req.user
-    const { phone } = req.body
+    const { phone, isDeactivated } = req.body
+
+    if (isDeactivated !== req.user.isDeactivated) {
+      const updatedUser = Object.assign(req.user, { isDeactivated })
+      MailService.createContact(updatedUser)
+    }
 
     try {
-      await Volunteer.updateOne({ _id }, { phone })
+      await Volunteer.updateOne({ _id }, { phone, isDeactivated })
       res.sendStatus(200)
     } catch (err) {
       next(err)
