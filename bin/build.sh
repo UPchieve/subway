c14af3712239a0ff0a8055f518850a9dbb9cccaf
Dave Sudia <dsudia@gospotcheck.com>
2021-01-07 18:54:57 -0700

    simplify pack build command

--- bin/build.sh
+++ bin/build.sh
@@ -18,32 +18,11 @@ image_latest="$CI_APPLICATION_REPOSITORY:latest"
 
 builder=${AUTO_DEVOPS_BUILD_IMAGE_CNB_BUILDER:-"heroku/buildpacks:18"}
 echo "Building Cloud Native Buildpack-based application with builder ${builder}..."
-buildpack_args=()
-if [[ -n "$BUILDPACK_URL" ]]; then
-  buildpack_args=('--buildpack' "$BUILDPACK_URL")
-fi
-env_args=()
-if [[ -n "$AUTO_DEVOPS_BUILD_IMAGE_FORWARDED_CI_VARIABLES" ]]; then
-  mapfile -t env_arg_names < <(echo "$AUTO_DEVOPS_BUILD_IMAGE_FORWARDED_CI_VARIABLES" | tr ',' "\n")
-  for env_arg_name in "${env_arg_names[@]}"; do
-    env_args+=('--env' "$env_arg_name")
-  done
-fi
 pack build "$image_tagged" \
   --builder "$builder" \
-  "${env_args[@]}" \
-  "${buildpack_args[@]}" \
   --buildpack registry.gitlab.com/upchieve/doppler-buildpack \
   --buildpack heroku/nodejs \
   --buildpack heroku/procfile \
-  --env HTTP_PROXY \
-  --env http_proxy \
-  --env HTTPS_PROXY \
-  --env https_proxy \
-  --env FTP_PROXY \
-  --env ftp_proxy \
-  --env NO_PROXY \
-  --env no_proxy \
   --env "NODE_ENV=production"
 
 docker tag "$image_tagged" "$image_latest"
