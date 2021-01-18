935419861e92b080de0c00c87ab7c97fa4fbac8b
Trey Stevens <treykstevens.tks@gmail.com>
2021-01-18 11:02:48 -0500

    adds custom web transactions to our socket events

--- router/api/sockets.js
+++ router/api/sockets.js
@@ -10,6 +10,7 @@ const SessionCtrl = require('../../controllers/SessionCtrl')
 const SocketService = require('../../services/SocketService')
 const QuillDocService = require('../../services/QuillDocService')
 const getSessionRoom = require('../../utils/get-session-room')
+const newrelic = require('newrelic')
 
 module.exports = function(io, sessionStore) {
   const socketService = new SocketService(io)
@@ -76,131 +77,192 @@ module.exports = function(io, sessionStore) {
 
     // Tutor session management
     socket.on('join', async function(data) {
-      if (!data || !data.sessionId) {
-        socket.emit('redirect')
-        return
-      }
+      newrelic.startWebTransaction(
+        '/socket-io/join',
+        () =>
+          new Promise(async (resolve, reject) => {
+            if (!data || !data.sessionId) {
+              socket.emit('redirect')
+              resolve()
+              return
+            }
 
-      const { sessionId } = data
-      const {
-        request: { user }
-      } = socket
-      let session
+            const { sessionId } = data
+            const {
+              request: { user }
+            } = socket
+            let session
 
-      try {
-        // @todo: have middleware handle the auth
-        if (!user) throw new Error('User not authenticated')
-        if (user.isVolunteer && !user.isApproved)
-          throw new Error('Volunteer not approved')
+            try {
+              // @todo: have middleware handle the auth
+              if (!user) throw new Error('User not authenticated')
+              if (user.isVolunteer && !user.isApproved)
+                throw new Error('Volunteer not approved')
 
-        session = await SessionModel.findById(sessionId)
-          .lean()
-          .exec()
-        if (!session) throw new Error('No session found!')
-      } catch (error) {
-        socket.emit('redirect')
-        return
-      }
+              session = await SessionModel.findById(sessionId)
+                .lean()
+                .exec()
+              if (!session) throw new Error('No session found!')
+            } catch (error) {
+              socket.emit('redirect')
+              reject(error)
+              return
+            }
 
-      try {
-        await SessionCtrl.join(socket, {
-          session,
-          user
-        })
+            try {
+              await SessionCtrl.join(socket, {
+                session,
+                user
+              })
 
-        const sessionRoom = getSessionRoom(sessionId)
-        const socketIds = await getSocketIdsFromRoom(user._id.toString())
-        // Have all of the user's socket connections join the tutoring session room
-        for (const id of socketIds) {
-          await remoteJoinRoom(id, sessionRoom)
-        }
+              const sessionRoom = getSessionRoom(sessionId)
+              const socketIds = await getSocketIdsFromRoom(user._id.toString())
+              // Have all of the user's socket connections join the tutoring session room
+              for (const id of socketIds) {
+                await remoteJoinRoom(id, sessionRoom)
+              }
 
-        socketService.emitSessionChange(sessionId)
-      } catch (error) {
-        socketService.bump(
-          socket,
-          {
-            endedAt: session.endedAt,
-            volunteer: session.volunteer || null,
-            student: session.student
-          },
-          error
-        )
-      }
+              socketService.emitSessionChange(sessionId)
+              resolve()
+            } catch (error) {
+              socketService.bump(
+                socket,
+                {
+                  endedAt: session.endedAt,
+                  volunteer: session.volunteer || null,
+                  student: session.student
+                },
+                error
+              )
+              reject(error)
+            }
+          })
+      )
     })
 
-    socket.on('list', async function() {
-      const sessions = await SessionModel.getUnfulfilledSessions()
-      socket.emit('sessions', sessions)
+    socket.on('list', () => {
+      newrelic.startWebTransaction(
+        '/socket-io/list',
+        () =>
+          new Promise(async (resolve, reject) => {
+            try {
+              const sessions = await SessionModel.getUnfulfilledSessions()
+              socket.emit('sessions', sessions)
+              resolve()
+            } catch (error) {
+              reject(error)
+            }
+          })
+      )
     })
 
-    socket.on('typing', function(data) {
-      socket.to(getSessionRoom(data.sessionId)).emit('is-typing')
+    socket.on('typing', data => {
+      newrelic.startWebTransaction('/socket-io/typing', () => {
+        socket.to(getSessionRoom(data.sessionId)).emit('is-typing')
+      })
     })
 
-    socket.on('notTyping', function(data) {
-      socket.to(getSessionRoom(data.sessionId)).emit('not-typing')
+    socket.on('notTyping', data => {
+      newrelic.startWebTransaction('/socket-io/notTyping', () => {
+        socket.to(getSessionRoom(data.sessionId)).emit('not-typing')
+      })
     })
 
-    socket.on('message', async function(data) {
-      const { user, sessionId, message } = data
-      // @todo: handle this differently?
-      if (!sessionId) return
+    socket.on('message', async data => {
+      newrelic.startWebTransaction(
+        '/socket-io/message',
+        () =>
+          new Promise(async (resolve, reject) => {
+            const { user, sessionId, message } = data
+            // @todo: handle this differently?
+            if (!sessionId) {
+              resolve()
+              return
+            }
 
-      try {
-        const newMessage = {
-          contents: message,
-          user: user._id,
-          createdAt: new Date()
-        }
-        await SessionCtrl.saveMessage({
-          sessionId: data.sessionId,
-          user: data.user,
-          message: newMessage
-        })
+            try {
+              const newMessage = {
+                contents: message,
+                user: user._id,
+                createdAt: new Date()
+              }
+              await SessionCtrl.saveMessage({
+                sessionId: data.sessionId,
+                user: data.user,
+                message: newMessage
+              })
 
-        const messageData = {
-          contents: newMessage.contents,
-          createdAt: newMessage.createdAt,
-          isVolunteer: user.isVolunteer,
-          userId: user._id
-        }
+              const messageData = {
+                contents: newMessage.contents,
+                createdAt: newMessage.createdAt,
+                isVolunteer: user.isVolunteer,
+                userId: user._id
+              }
 
-        const socketRoom = getSessionRoom(data.sessionId)
-        io.in(socketRoom).emit('messageSend', messageData)
-      } catch (error) {
-        socket.emit('messageError')
-      }
+              const socketRoom = getSessionRoom(data.sessionId)
+              io.in(socketRoom).emit('messageSend', messageData)
+              resolve()
+            } catch (error) {
+              socket.emit('messageError')
+              reject(error)
+            }
+          })
+      )
     })
 
     socket.on('requestQuillState', async ({ sessionId }) => {
-      let docState = await QuillDocService.getDoc(sessionId)
-      if (!docState) docState = await QuillDocService.createDoc(sessionId)
-      socket.emit('quillState', {
-        delta: docState
-      })
+      newrelic.startWebTransaction(
+        '/socket-io/requestQuillState',
+        () =>
+          new Promise(async (resolve, reject) => {
+            try {
+              let docState = await QuillDocService.getDoc(sessionId)
+              if (!docState)
+                docState = await QuillDocService.createDoc(sessionId)
+              socket.emit('quillState', {
+                delta: docState
+              })
+              resolve()
+            } catch (error) {
+              reject(error)
+            }
+          })
+      )
     })
 
     socket.on('transmitQuillDelta', async ({ sessionId, delta }) => {
-      QuillDocService.appendToDoc(sessionId, delta)
-      socket.to(getSessionRoom(sessionId)).emit('partnerQuillDelta', {
-        delta
-      })
+      newrelic.startWebTransaction(
+        '/socket-io/transmitQuillDelta',
+        () =>
+          new Promise(async (resolve, reject) => {
+            QuillDocService.appendToDoc(sessionId, delta)
+            socket.to(getSessionRoom(sessionId)).emit('partnerQuillDelta', {
+              delta
+            })
+            resolve()
+          })
+      )
     })
 
     socket.on('transmitQuillSelection', async ({ sessionId, range }) => {
-      socket.to(getSessionRoom(sessionId)).emit('quillPartnerSelection', {
-        range
+      newrelic.startWebTransaction('/socket-io/transmitQuillSelection', () => {
+        socket.to(getSessionRoom(sessionId)).emit('quillPartnerSelection', {
+          range
+        })
       })
     })
 
     socket.on('error', function(error) {
-      console.log('Socket error: ', error)
-      Sentry.captureException(error)
+      newrelic.startWebTransaction('/socket-io/error', () => {
+        console.log('Socket error: ', error)
+        Sentry.captureException(error)
+      })
     })
 
     socket.on('resetWhiteboard', async ({ sessionId }) => {
-      socket.to(getSessionRoom(sessionId)).emit('resetWhiteboard')
+      newrelic.startWebTransaction('/socket-io/resetWhiteboard', () => {
+        socket.to(getSessionRoom(sessionId)).emit('resetWhiteboard')
+      })
     })
   })
 }
