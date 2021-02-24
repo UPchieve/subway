8b4d3004cda7270047d1ef18a7d333ce6a38398c
Dave Sudia <dsudia@gospotcheck.com>
2021-02-23 19:59:59 -0700

    add error handling to VersionService

--- src/services/VersionService.js
+++ src/services/VersionService.js
@@ -1,8 +1,25 @@
 import NetworkService from './NetworkService'
+import * as Sentry from '@sentry/browser'
 
 export default {
   async getCurrentVersion() {
-    const checkHealthResponse = await NetworkService.checkHealth()
-    return checkHealthResponse.body.version
+    // default this to 'unknown' so if another request
+    // succeeds, the user will be prompted to refresh
+    let version
+    let checkHealthResponse
+    try {
+      checkHealthResponse = await NetworkService.checkHealth()
+      if (!Object.prototype.call(hasOwnProperty, 'body')) {
+        throw new Error('healthz response has no body property')
+      }
+      if (!Object.prototype.call(hasOwnProperty, 'version')) {
+        throw new Error('healthz response body has no version property')
+      }
+      version = checkHealthResponse.body.version
+    } catch (err) {
+      version = 'unknown'
+      Sentry.captureException(err)
+    }
+    return version
   }
 }
