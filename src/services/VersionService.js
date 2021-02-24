fe42b1f7a6f66db737134a573810a25b52ac52d3
Dave Sudia <dsudia@gospotcheck.com>
2021-02-23 20:13:24 -0700

    change name of Version service method

--- src/services/VersionService.js
+++ src/services/VersionService.js
@@ -2,7 +2,7 @@ import NetworkService from './NetworkService'
 import * as Sentry from '@sentry/browser'
 
 export default {
-  async getCurrentVersion() {
+  async getCurrentServerVersion() {
     // default this to 'unknown' so if another request
     // succeeds, the user will be prompted to refresh
     let version
