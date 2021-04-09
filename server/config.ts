f46b7bed13452a153c07762d487a5842dfd04f7a
Dave Sudia <dave.sudia@upchieve.org>
2021-04-09 17:30:33 -0600

    remove the allowedUSername URI Redis setting

--- server/config.ts
+++ server/config.ts
@@ -29,7 +29,7 @@ if (process.env.SUBWAY_REDIS_USE_SECURE) {
   const redisUsername = process.env.SUBWAY_REDIS_USERNAME || ''
   const redisPassword = process.env.SUBWAY_REDIS_PASSWORD || ''
   // https://github.com/luin/ioredis#connect-to-redis
-  redisConn = `rediss://${redisUsername}:${redisPassword}@${redisHost}:${redisPort}/?allowUsernameInURI=true`
+  redisConn = `rediss://${redisUsername}:${redisPassword}@${redisHost}:${redisPort}`
 } else {
   redisConn = `redis://${redisHost}:${redisPort}`
 }
