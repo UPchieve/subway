35113185693cd865fd1a8eb96308b3a036a10391
Trey Stevens <treykstevens.tks@gmail.com>
2021-04-20 17:10:51 +0000

    backfill elapsed availability + queue email scripts

---
+++ server/dbutils/657-incident-worker-outage/657-incident-backfill-elapsed-availability.ts
@@ -0,0 +1,162 @@
+import mongoose from 'mongoose'
+import dbconnect from '../dbconnect'
+import { AvailabilitySnapshot } from '../../models/Availability/Snapshot'
+import VolunteerModel from '../../models/Volunteer'
+import { USER_ACTION } from '../../constants'
+import moment from 'moment-timezone'
+import {
+  createAvailabilityHistory,
+  getAvailability,
+  getElapsedAvailability
+} from '../../services/AvailabilityService'
+import logger from '../../logger'
+
+/**
+ *
+ * Incident Range (UTC):
+ * 2021-04-10T13:30:00.000+00:00
+ * 2021-04-14T03:44:00.000+00:00
+ *
+ * Availability history snapshot dates not stored in the database:
+ * 2021-04-10T23:59:59.999+00:00
+ * 2021-04-11T23:59:59.999+00:00
+ * 2021-04-12T23:59:59.999+00:00
+ *
+ * Notes:
+ * UpdateElapsedAvailability is a daily cron job processed at 08:00 (UTC)
+ *
+ * 04/14 at 03:44:00 UTC is when the worker queue started working
+ * again and UpdatedElapsedAvailability was fired off and created new
+ * availability history snapshots only for 04/13.
+ *
+ *
+ * Steps:
+ * 1. Get all volunteers who are onboarded and approved (ready to coach)
+ * 2: If the volunteer was ready to coach inbetween the incident range:
+ *      - Only add history snapshots from the date they became ready to coach
+ * 3. Create new history snapshots for 4/10, 4/11, and 4/12
+ * 4. Increment the volunteer's total elapsed availability by the amount
+ *    elapsed availability passed for that day
+ *
+ */
+
+const startOfMissingHistorySnapshots = new Date('2021-04-10T00:00:00.000+00:00')
+const endOfMissingHistorySnapshots = new Date('2021-04-13T00:00:00.000+00:00')
+
+const main = async (): Promise<void> => {
+  try {
+    await dbconnect()
+
+    const volunteers: any = await VolunteerModel.aggregate([
+      {
+        $match: {
+          isOnboarded: true,
+          isApproved: true
+        }
+      },
+      {
+        $lookup: {
+          from: 'useractions',
+          let: { userId: '$_id' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ['$user', '$$userId']
+                }
+              }
+            },
+            {
+              $sort: {
+                createdAt: -1
+              }
+            }
+          ],
+          as: 'userActions'
+        }
+      },
+      {
+        $addFields: {
+          actions: {
+            $filter: {
+              input: '$userActions',
+              as: 'userAction',
+              cond: {
+                $or: [
+                  {
+                    $eq: ['$$userAction.action', USER_ACTION.ACCOUNT.APPROVED]
+                  },
+                  {
+                    $eq: ['$$userAction.action', USER_ACTION.ACCOUNT.ONBOARDED]
+                  }
+                ]
+              }
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          readyToCoachDate: { $arrayElemAt: ['$actions.createdAt', 0] }
+        }
+      }
+    ])
+
+
+    for (const volunteer of volunteers) {
+      const availability: AvailabilitySnapshot = await getAvailability({
+        volunteerId: volunteer._id
+      })
+      if (!availability) continue
+
+      // There are legacy volunteers who are onboarded and approved (ready to coach), but
+      // pre-date us tracking when they got onboarded and approved
+      const legacyReadyToCoachDate = new Date('2020-01-01T00:00:00.000+00:00')
+      if (!volunteer.readyToCoachDate)
+        volunteer.readyToCoachDate = legacyReadyToCoachDate
+
+      const readyToCoachDate = new Date(
+        moment(volunteer.readyToCoachDate)
+          .utc()
+          .startOf('day')
+      ).getTime()
+
+      let currentDate = startOfMissingHistorySnapshots.getTime()
+      while (currentDate < endOfMissingHistorySnapshots.getTime()) {
+        if (readyToCoachDate <= currentDate) {
+          const endOfDate = moment(currentDate)
+            .utc()
+            .endOf('day')
+          const day = moment(currentDate)
+            .utc()
+            .format('dddd')
+          const availabilityDay = availability.onCallAvailability[day]
+          const elapsedAvailability = getElapsedAvailability(availabilityDay)
+
+          await VolunteerModel.updateOne(
+            {
+              _id: volunteer._id
+            },
+            { $inc: { elapsedAvailability } }
+          )
+
+          const newAvailabilityHistory = {
+            availability: availabilityDay,
+            volunteerId: volunteer._id,
+            timezone: availability.timezone,
+            date: endOfDate
+          }
+          await createAvailabilityHistory(newAvailabilityHistory)
+        }
+        currentDate = new Date(moment(currentDate).add(1, 'days')).getTime()
+      }
+    }
+  } catch (error) {
+    logger.error(error)
+  }
+  logger.info('Disconnecting')
+  mongoose.disconnect()
+}
+
+main()
---
+++ server/dbutils/657-incident-worker-outage/657-incident-remove-duplicate-elapsed-availability.ts
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose'
+import dbconnect from '../dbconnect'
+import VolunteerModel from '../../models/Volunteer'
+import logger from '../../logger'
+import { getElapsedAvailability } from '../../services/AvailabilityService'
+import AvailabilityHistoryModel from '../../models/Availability/History'
+
+/**
+ *
+ * Incident Range (UTC):
+ * 2021-04-10T13:30:00.000+00:00
+ * 2021-04-14T03:44:00.000+00:00
+ *
+ * Duplicate availability history snapshots stored:
+ * 2021-04-13T23:59:59.999+00:00
+ * 2021-04-13T23:59:59.999+00:00
+ *
+ * Notes:
+ * UpdateElapsedAvailability is a daily cron job processed at 08:00 (UTC)
+ *
+ * 04/14 at 03:44:00 UTC is when the worker queue started working 
+ * again and UpdatedElapsedAvailability was fired off and created new
+ * availability history snapshots only for 04/13.
+ *
+ * A few hours later, the cron job was executed again.
+ * Because UpdateElapsedAvailability was ran twice in one day, there
+ * are currenly duplicate history snapshot entries for 04/13 and
+ * the elapsed availability for that day was added to a volunteer's
+ * total elapsed availability twice
+ *
+ * Steps:
+ * 1. Per duplicate availability history snapshot for date 04/13 remove
+ *    the amount of elapsed availability from the volunteer
+ * 2. Remove the duplicate availability history snapshot for date 04/13
+ *
+ */
+const main = async (): Promise<void> => {
+  try {
+    await dbconnect()
+
+    const availabilityHistoryDocs = await AvailabilityHistoryModel.find({
+      date: new Date('2021-04-13T23:59:59.999+00:00'),
+      createdAt: { $lt: new Date('2021-04-14T06:00:00.000+00:00') }
+    })
+      .lean()
+      .exec()
+
+    for (const doc of availabilityHistoryDocs) {
+      // elapsedAvailabilityToRemove will be a negative number
+      const elapsedAvailabilityToRemove =
+        Math.abs(getElapsedAvailability(doc.availability)) * -1
+      await VolunteerModel.updateOne(
+        { _id: doc.volunteerId },
+        { $inc: { elapsedAvailability: elapsedAvailabilityToRemove } }
+      )
+    }
+
+    await AvailabilityHistoryModel.deleteMany({
+      date: new Date('2021-04-13T23:59:59.999+00:00'),
+      // delete the docs that were created before the cron job ran again
+      // on the same date at 2021-04-14T06:00:00.000+00:00
+      createdAt: { $lt: new Date('2021-04-14T06:00:00.000+00:00') }
+    })
+  } catch (error) {
+    logger.error(error)
+  }
+  // Only disconnect after uploading has finished
+  logger.info('Disconnecting')
+  mongoose.disconnect()
+}
+
+main()
