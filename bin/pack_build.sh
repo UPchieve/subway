8d2b4acc58b76fda0a57b23dbd88335a73a9a57c
Dave Sudia <dave.sudia@upchieve.org>
2021-05-06 14:15:05 -0600

    prunes dev dependencies in build

--- bin/pack_build.sh
+++ bin/pack_build.sh
@@ -4,5 +4,4 @@ pack build "upchieve/subway:local" \
   --builder heroku/buildpacks:20 \
   --buildpack registry.gitlab.com/upchieve/doppler-buildpack \
   --buildpack heroku/nodejs \
-  --buildpack heroku/procfile@0.6.2 \
-  --env "NODE_ENV=production"
+  --buildpack heroku/procfile@0.6.2
