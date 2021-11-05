338bef0e2ad9d2ea351a9e349d58191fa36aceab
Trey Stevens <treykstevens.tks@gmail.com>
2021-11-05 20:25:46 +0000

    Gates account creation

--- server/services/listeners/GatesStudyServiceListeners.ts
+++ server/services/listeners/GatesStudyServiceListeners.ts
@@ -1,11 +1,11 @@
-import { SESSION_EVENTS } from '../../constants/events'
+import { STUDENT_EVENTS } from '../../constants/events'
 import * as GatesStudyService from '../GatesStudyService'
 import register from './register'
 
 export function listeners() {
   register(
-    SESSION_EVENTS.SESSION_ENDED,
-    GatesStudyService.processGatesQualifiedSession,
-    'processGatesQualifiedSession'
+    STUDENT_EVENTS.STUDENT_CREATED,
+    GatesStudyService.processGatesQualifiedCheck,
+    'processGatesQualifiedCheck'
   )
 }
--- server/utils/gates-study-utils.ts
+++ server/utils/gates-study-utils.ts
@@ -1,7 +1,5 @@
 import { Types } from 'mongoose'
-import { Session } from '../models/Session'
-import { MATH_SUBJECTS, GRADES } from '../constants'
-import { getSessionById } from '../models/Session/queries'
+import { GRADES } from '../constants'
 import { getStudentById } from '../models/Student/queries'
 import { getSchool } from '../services/SchoolService'
 import { Student } from '../models/Student'
@@ -9,49 +7,38 @@ import { School } from '../models/School'
 import { getIdFromModelReference } from '../utils/model-reference'
 
 export interface GatesQualifiedData {
-  session: Session
   student: Student
   school: School
 }
 
 /**
- * A Gates-qualified session is defined as a session with the following:
+ * A Gates-qualified student is defined as a student with the following:
  *
  * - Not a school partner student
  * - Not a nonprofit partner student
- * - Brand new student, must be first session on the platform
  * - In 9th grade or 10th grade
- * - Not reported by time of ending
- * - Math tutoring session
  *
  */
-export function isGatesQualifiedSession(data: GatesQualifiedData) {
-  const { session, student, school } = data
-
+export function isGatesQualifiedStudent(data: GatesQualifiedData) {
+  const { student, school } = data
   return (
     !school.isPartner &&
     !student.studentPartnerOrg &&
-    student.pastSessions.length === 1 &&
     (student.currentGrade === GRADES.NINTH ||
-      student.currentGrade === GRADES.TENTH) &&
-    !session.isReported &&
-    // must typecast values to string since typescript treats enum RHS values weirdly
-    Object.values<string>(MATH_SUBJECTS).includes(session.subTopic)
+      student.currentGrade === GRADES.TENTH)
   )
 }
 
 export async function prepareForGatesQualificationCheck(
-  sessionId: Types.ObjectId
+  userId: Types.ObjectId
 ): Promise<GatesQualifiedData> {
-  const session = await getSessionById(sessionId)
-  const student = await getStudentById(getIdFromModelReference(session.student))
+  const student = await getStudentById(userId)
   if (!student) throw new Error('Gates student not found')
   const school = await getSchool(
     getIdFromModelReference(student.approvedHighschool)
   )
   if (!school) throw new Error('Gates school not found')
   return {
-    session,
     student,
     school,
   }
