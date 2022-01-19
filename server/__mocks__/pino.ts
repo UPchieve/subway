2b326d984fafdb7286c070700b8c61c42102b26f
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-01-19 15:20:00 +0000

    Typed manual npm module mocks

--- server/__mocks__/pino.ts
+++ server/__mocks__/pino.ts
@@ -1,16 +1,14 @@
-'use strict'
-
 /**
  * Mock the pino logger to avoid needing to call jest.mock(logger) in every test
  *
  * TODO: redirect test logs to some local file to not clutter output
  */
 
-module.exports = function pino(options) {
+export default function pino(options: unknown) {
   return {
     info: jest.fn(),
     error: jest.fn(),
     debug: jest.fn(),
-    warn: jest.fn()
+    warn: jest.fn(),
   }
 }
