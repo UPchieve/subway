b56308390bec22239e7495bb629ff24400b1bc28
Trey Stevens <treykstevens.tks@gmail.com>
2022-03-03 17:37:51 +0000

    add student_favorite_volunteers table

---
+++ database/migrations/20220223184006_create_student_favorite_volunteers.sql
@@ -0,0 +1,17 @@
+-- migrate:up
+CREATE TABLE upchieve.student_favorite_volunteers (
+    student_id uuid NOT NULL,
+    volunteer_id uuid NOT NULL,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz NOT NULL
+);
+ALTER TABLE ONLY upchieve.student_favorite_volunteers
+    ADD CONSTRAINT student_favorite_volunteers_student_id_fkey FOREIGN KEY (student_id) REFERENCES upchieve.users(id),
+    ADD CONSTRAINT student_favorite_volunteers_volunteer_id_fkey FOREIGN KEY (volunteer_id) REFERENCES upchieve.users(id);
+
+
+-- migrate:down
+ALTER TABLE upchieve.student_favorite_volunteers
+  DROP CONSTRAINT student_favorite_volunteers_student_id_fkey,
+  DROP CONSTRAINT student_favorite_volunteers_volunteer_id_fkey;
+DROP TABLE IF EXISTS upchieve.student_favorite_volunteers CASCADE;
