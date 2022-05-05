7edfde6e60a28c187d809d102d3dc62660b941f6
Fern Sanchez <fernando.sanchez@upchieve.org>
2022-05-05 17:55:11 +0000

    Create zip code check route

--- server/router/eligibility/index.ts
+++ server/router/eligibility/index.ts
@@ -2,41 +2,16 @@ import express, { Express, Router } from 'express'
 import * as Sentry from '@sentry/node'
 import { authPassport } from '../../utils/auth-utils'
 import * as SchoolService from '../../services/SchoolService'
-import * as UserCtrl from '../../controllers/UserCtrl'
-import { findSchoolByUpchieveId } from '../../models/School/queries'
 import { getZipCodeByZipCode } from '../../models/ZipCode/queries'
-import {
-  getIneligibleStudentByEmail,
-  getIneligibleStudentsPaginated,
-  insertIneligibleStudent,
-} from '../../models/IneligibleStudent/queries'
+import { getIneligibleStudentsPaginated } from '../../models/IneligibleStudent/queries'
 import { resError } from '../res-error'
 import * as IpAddressService from '../../services/IpAddressService'
-import { getUserIdByEmail } from '../../models/User/queries'
+import { asString, asUlid, asBoolean } from '../../utils/type-utils'
 import {
-  asFactory,
-  asString,
-  asEnum,
-  asOptional,
-  asUlid,
-  asBoolean,
-} from '../../utils/type-utils'
-import { GRADES } from '../../constants'
-
-interface CheckEligibilityPayload {
-  schoolUpchieveId: string
-  zipCode: string
-  email: string
-  referredByCode?: string
-  currentGrade?: GRADES
-}
-const asCheckEligibilityPayload = asFactory<CheckEligibilityPayload>({
-  schoolUpchieveId: asString,
-  zipCode: asString,
-  email: asString,
-  referredByCode: asOptional(asString),
-  currentGrade: asOptional(asEnum(GRADES)),
-})
+  checkEligibility,
+  ExistingUserError,
+  checkZipCode,
+} from '../../services/EligibilityService'
 
 export function routes(app: Express) {
   const router: Router = express.Router()
@@ -44,51 +19,11 @@ export function routes(app: Express) {
   // Check if a student is eligible
   router.route('/check').post(async function(req, res) {
     try {
-      const {
-        schoolUpchieveId,
-        zipCode: zipCodeInput,
-        email,
-        referredByCode,
-        currentGrade,
-      } = asCheckEligibilityPayload(req.body as unknown)
-
-      const existingUser = await getUserIdByEmail(email)
-      if (existingUser)
-        return res.status(422).json({
-          message: 'Email already in use',
-        })
-
-      const existingIneligible = await getIneligibleStudentByEmail(email)
-      if (existingIneligible) return res.json({ isEligible: false })
-
-      const school = await findSchoolByUpchieveId(schoolUpchieveId)
-      const zipCode = await getZipCodeByZipCode(zipCodeInput)
-
-      const isSchoolApproved = !!school && school.isApproved
-      const isZipCodeEligible = !!zipCode && zipCode.isEligible
-      const isCollegeStudent = currentGrade === GRADES.COLLEGE ? true : false
-      const isStudentEligible =
-        (isSchoolApproved || isZipCodeEligible) && !isCollegeStudent
-
-      if (!isStudentEligible) {
-        const referredBy = await UserCtrl.checkReferral(referredByCode)
-        await insertIneligibleStudent(
-          email,
-          school?.id,
-          zipCodeInput,
-          currentGrade,
-          referredBy,
-          req.ip
-        )
-      }
-
-      if (isCollegeStudent)
-        return res.json({
-          isEligible: isStudentEligible,
-          isCollegeStudent: isCollegeStudent,
-        })
-      else return res.json({ isEligible: isStudentEligible })
+      const result = await checkEligibility(req.ip, req.body as unknown)
+      return res.json(result)
     } catch (err) {
+      if (err instanceof ExistingUserError)
+        return res.status(422).json({ message: 'Email already in use' })
       resError(res, err)
     }
   })
@@ -233,6 +168,16 @@ export function routes(app: Express) {
     try {
       await IpAddressService.checkIpAddress(req.ip)
       res.sendStatus(200)
+    } catch (err) {
+      resError(res, err)
+    }
+  })
+
+  router.get('/check-zip-code/:zipCode', async function(req, res) {
+    try {
+      const zipCode = asString(req.params.zipCode)
+      const result = await checkZipCode(zipCode)
+      res.json({ isValidZipCode: result })
     } catch (err) {
       resError(res, err)
     }
