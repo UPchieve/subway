ddced411553f7bec2bcea7c0a7f281da8e9202a4
Haley Brandt-Erichsen <haley.brandterichsen@upchieve.org>
2022-06-21 11:28:40 -0400

    1074: script code review

--- server/scripts/delete-duplicate-feedbacks.ts
+++ server/scripts/delete-duplicate-feedbacks.ts
@@ -1,16 +1,9 @@
-import * as db from '../db'
 import { removeDuplicateFeedbacks } from '../models/Feedback/queries'
 
 export default async function DeleteDuplicateFeedbacks() {
-  let exitCode = 0
   try {
-    await db.connect()
     await removeDuplicateFeedbacks()
   } catch (error) {
     console.log(`Uncaught error: ${error}`)
-    exitCode = 1
-  } finally {
-    await db.closeClient()
-    process.exit(exitCode)
   }
 }
