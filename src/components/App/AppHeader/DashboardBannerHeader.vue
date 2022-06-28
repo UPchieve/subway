15065f2806f4dd9327ecfb3119fb4feaa55184b2
Heather <heather@Heathers-MacBook-Air.local>
2022-06-28 13:14:15 -0400

    automate us history dashboard banner text to change after launched to students

--- src/components/App/AppHeader/DashboardBannerHeader.vue
+++ src/components/App/AppHeader/DashboardBannerHeader.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="header">
     <!-- Edit the text below to display on dashboard banner to volunteers -->
-    <div class="header-message"><span>Introducing our new subject: U.S. History!</span> Take the quiz by July 15th to help us launch to students.</div>
+    <!-- Show Below Message Until July 15th: -->
+    <div v-if="new Date() < new Date('7-15-2022')" class="header-message"><span>Introducing our new subject: U.S. History!</span> Take the quiz by July 15th to help us launch to students.</div>
+    <!-- Show Below Message starting July 15th -->
+    <div v-if="new Date() >= new Date('7-15-2022')" class="header-message"><span>Introducing our new subject: U.S. History!</span> Take the quiz today.</div>
     <router-link to="/training" tag="button" class="header-button">Get Certified</router-link>
   </div>
 </template>
