38dbd46cecfb46dc963b7a9f83ceb96e835e5271
Dave Sudia <dave.sudia@upchieve.org>
2022-08-19 12:11:31 -0600

    removes territory exclusion when seeding - we have accurate income data

--- database/seeds/static/geography/postal-codes/postal-codes.ts
+++ database/seeds/static/geography/postal-codes/postal-codes.ts
@@ -19,25 +19,11 @@ export async function postalCodes(numZipCodes: number | undefined) {
   })
   const recordInsertions = zipRecords.slice(0, numZipCodes).map((record: csvPostalCodeRecord) => {
     const typedRecord = record as csvPostalCodeRecord
-    const excludedTerritories = [
-      'VI',
-      'GU',
-      'AE',
-      'AA',
-      'AP',
-      'AS',
-      'PR',
-      'PW',
-      'FM',
-      'MP',
-      'MH',
-    ]
-    if (excludedTerritories.includes(record.state)) return Promise.resolve()
     return wrapInsert('postal_codes', pgQueries.insertZipCode.run, {
       code: typedRecord.zipcode,
       usStateCode: typedRecord.state,
       income: typedRecord.income,
-      lattitude: typedRecord.latitude,
+      latitude: typedRecord.latitude,
       longitude: typedRecord.longitude,
     })
   })
@@ -46,7 +32,7 @@ export async function postalCodes(numZipCodes: number | undefined) {
     code: '00000',
     usStateCode: 'NA',
     income: 0,
-    lattitude: 0,
+    latitude: 0,
     longitude: 0
   })
 }
