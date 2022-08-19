efd519cc9b1f7c613d38f1743c59c5cc8c55d542
Dave Sudia <dave.sudia@upchieve.org>
2022-08-19 13:08:23 -0600

    fix file reference for aggregated_data in upsert job

--- server/scripts/upsert-postal-codes.ts
+++ server/scripts/upsert-postal-codes.ts
@@ -6,7 +6,7 @@ import parse from 'csv-parse/lib/sync'
 export default async function upsertPostalCodes(): Promise<void> {
   try {
     const zipFile = fs.readFileSync(
-      `${__dirname}/../../database/seeds/geography/postal-codes/aggregated_data.csv`
+      `${__dirname}/../../database/seeds/static/geography/postal-codes/aggregated_data.csv`
     )
     const zipRecords: ZCRepo.csvPostalCodeRecord[] = await parse(zipFile, {
       delimiter: ',',
