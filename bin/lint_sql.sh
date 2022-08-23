9eb88d8f1c6543fa984dce7b00bca3aee501eb95
Dave Sudia <dave.sudia@upchieve.org>
2022-08-23 11:45:33 -0600

    adds pre-commit pkg and list of lint commands to run

--- bin/lint_sql.sh
+++ bin/lint_sql.sh
@@ -1,6 +1,25 @@
 #! /usr/bin/env bash
 
-for filename in server/**/**/*.sql; do
+echo "linting files in server/"
+for filename in $(find ./server -name "*.sql"); do
     [ -e "$filename" ] || continue
     npx pg-formatter --keyword-case="uppercase" --inplace --placeholder=":\w+!" "$filename"
+done
+
+echo "linting files in database/migrations"
+for filename in $(find ./database/migrations -name "*.sql"); do
+  [ -e "$filename" ] || continue
+  npx pg-formatter --keyword-case="uppercase" --inplace --placeholder=":\w+!" "$filename"
+done
+
+echo "linting files in database/seed-updates"
+for filename in $(find ./database/seed-updates -name "*.sql"); do
+  [ -e "$filename" ] || continue
+  npx pg-formatter --keyword-case="uppercase" --inplace --placeholder=":\w+!" "$filename"
+done
+
+echo "linting files in database/seeds"
+for filename in $(find ./database/seed-updates -name "*.sql"); do
+  [ -e "$filename" ] || continue
+  npx pg-formatter --keyword-case="uppercase" --inplace --placeholder=":\w+!" "$filename"
 done
