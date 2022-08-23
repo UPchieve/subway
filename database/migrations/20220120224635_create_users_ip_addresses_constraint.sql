10f92b0e3bb2c6fffbdadb8dac206db6d182e599
Dave Sudia <dave.sudia@upchieve.org>
2022-08-23 15:46:06 -0600

    add missing semicolon to migration

--- database/migrations/20220120224635_create_users_ip_addresses_constraint.sql
+++ database/migrations/20220120224635_create_users_ip_addresses_constraint.sql
@@ -1,6 +1,8 @@
 -- migrate:up
 ALTER TABLE upchieve.users_ip_addresses
-    ADD CONSTRAINT unique_user_id_ip_address_id UNIQUE (user_id, ip_address_id)
-    -- migrate:down
-        ALTER TABLE upchieve.users_ip_addresses
-        DROP CONSTRAINT unique_user_id_ip_address_id
+    ADD CONSTRAINT unique_user_id_ip_address_id UNIQUE (user_id, ip_address_id);
+
+-- migrate:down
+ALTER TABLE upchieve.users_ip_addresses
+    DROP CONSTRAINT unique_user_id_ip_address_id;
+
