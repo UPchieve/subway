3e817af4b8b8025e3b50f242a0be32a74f917a8e
Alex Lindsay <alex.lindsay@upchieve.org>
2023-08-16 15:53:17 +0000

    Add createStudentProfile repository method.

--- server/models/Student/types.ts
+++ server/models/Student/types.ts
@@ -1,4 +1,5 @@
-import { Pgid, Ulid } from '../pgUtils'
+import { GRADES } from '../../constants'
+import { Ulid, Uuid } from '../pgUtils'
 
 export type Student = {
   userId: Ulid
@@ -11,4 +12,77 @@ export type Student = {
   studentPartnerOrgSiteId?: Ulid
   createdAt: Date
   updatedAt: Date
+}
+
+export type CreateStudentPayload = {
+  email: string
+  firstName: string
+  lastName: string
+  password?: string | undefined
+  referredBy: Ulid | undefined
+  studentPartnerOrg?: string | undefined
+  zipCode?: string | undefined
+  approvedHighschool: Ulid | undefined
+  currentGrade?: string
+  partnerSite?: string
+  partnerUserId?: string
+  college?: string
+  signupSourceId?: number
+  otherSignupSource?: string
+  verified?: boolean
+  emailVerified?: boolean
+}
+
+export type CreateStudentWithPasswordPayload = CreateStudentPayload & {
+  password: string
+}
+export type CreateStudentWithFedCredPayload = CreateStudentPayload & {
+  password?: string | undefined
+  verified: boolean
+  emailVerified: boolean
+}
+
+export type CreatedStudent = StudentContactInfo & {
+  isDeactivated: boolean
+  isTestUser: boolean
+  createdAt: Date
+  isVolunteer: boolean
+  isAdmin: boolean
+  isBanned: boolean
+  verified: boolean
+  zipCode?: string
+  currentGrade?: string
+  lastname: string
+  firstname: string
+}
+
+export type StudentContactInfo = {
+  id: Ulid
+  firstName: string
+  email: string
+  studentPartnerOrg?: string
+  schoolId?: Ulid
+}
+
+export type CreateStudentProfilePayload = {
+  college?: string
+  gradeLevel?: string
+  partnerSite?: string
+  schoolId?: Ulid
+  studentPartnerOrg?: string
+  userId: Ulid
+  zipCode?: string
+}
+
+export type CreateStudentProfileResult = {
+  college?: string
+  createdAt: Date
+  gradeLevel?: GRADES
+  partnerSite?: string
+  partnerUserId?: string
+  postalCode?: string
+  schoolId?: Uuid
+  studentPartnerOrg?: string
+  updatedAt: Date
+  userId: Ulid
 }
