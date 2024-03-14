9356c0b736d159e012cef0a941ba9e2b3dbd9e0a
sammy nave <sammy.nave@gmail.com>
2024-03-14 19:45:22 +0000

    Queue up individual email summary jobs

--- server/worker/jobs/emailWeeklyHourSummary.ts
+++ server/worker/jobs/emailWeeklyHourSummary.ts
@@ -1,35 +1,28 @@
 import moment from 'moment'
 import 'moment-timezone'
-import { log } from '../logger'
 import { getHourSummaryStats } from '../../services/VolunteerService'
 import * as MailService from '../../services/MailService'
 import config from '../../config'
 import { telecomHourSummaryStats } from '../../utils/reportUtils'
-import { Jobs } from '.'
 import {
-  getVolunteersForWeeklyHourSummary,
+  VolunteerForWeeklyHourSummary,
   updateVolunteerHourSummaryIntroById,
 } from '../../models/Volunteer/queries'
-import newrelic from 'newrelic'
 import { getWeeklySummaryAllHoursFlag } from '../../services/FeatureFlagService'
+import { Job } from 'bull'
+import { ISOString } from '../../constants'
+import { Jobs } from '.'
+interface EmailWeeklyHourSummaryJobData {
+  startDate: ISOString
+  endDate: ISOString
+  volunteer: VolunteerForWeeklyHourSummary
+}
 
-// Runs weekly at 6am EST on Monday
-export default async (): Promise<void> => {
-  //  Monday-Sunday
-  const lastMonday = moment()
-    .utc()
-    .subtract(1, 'weeks')
-    .startOf('isoWeek')
-  const lastSunday = moment()
-    .utc()
-    .subtract(1, 'weeks')
-    .endOf('isoWeek')
-
-  const volunteers = await getVolunteersForWeeklyHourSummary()
-
-  let totalEmailed = 0
-  const errors: string[] = []
-  for (const volunteer of volunteers) {
+export default async (
+  job: Job<EmailWeeklyHourSummaryJobData>
+): Promise<void> => {
+  const { volunteer, startDate, endDate } = job.data
+  try {
     const {
       id,
       firstName,
@@ -37,32 +30,26 @@ export default async (): Promise<void> => {
       sentHourSummaryIntroEmail,
       volunteerPartnerOrg,
     } = volunteer
-    try {
-      const customCheck = config.customVolunteerPartnerOrgs.some(
-        org => org === volunteerPartnerOrg
+    const start = moment(startDate).utc()
+    const end = moment(endDate).utc()
+    const customCheck = config.customVolunteerPartnerOrgs.some(
+      org => org === volunteerPartnerOrg
+    )
+    let summaryStats
+    if (volunteer.sentHourSummaryIntroEmail === undefined) return
+    if (customCheck)
+      summaryStats = await telecomHourSummaryStats(
+        volunteer,
+        start.toDate(),
+        end.toDate()
       )
-      let summaryStats
-      if (volunteer.sentHourSummaryIntroEmail === undefined) continue
-      if (customCheck)
-        summaryStats = await telecomHourSummaryStats(
-          volunteer,
-          lastMonday.toDate(),
-          lastSunday.toDate()
-        )
-      else
-        summaryStats = await getHourSummaryStats(
-          id,
-          lastMonday.toDate(),
-          lastSunday.toDate()
-        )
+    else
+      summaryStats = await getHourSummaryStats(id, start.toDate(), end.toDate())
 
-      const isWeeklySummaryAllHoursActive = await getWeeklySummaryAllHoursFlag(
-        id
-      )
-      const allowZeroHours =
-        isWeeklySummaryAllHoursActive && !volunteerPartnerOrg
+    const isWeeklySummaryAllHoursActive = await getWeeklySummaryAllHoursFlag(id)
+    const allowZeroHours = isWeeklySummaryAllHoursActive && !volunteerPartnerOrg
 
-      /*
+    /*
       The smallest this number can be is .01 hours =36 seconds (as per the rounding
       in VolunteerService.ts:68-70) So users with 36-54 seconds of time will have
       .01 hours coaching which gets rounded down to 0 hours/minutes at formatting
@@ -70,39 +57,29 @@ export default async (): Promise<void> => {
       to 0 prevent an email from getting sent that displays 0 hours of volutneering
       TODO: clean up formatting rounding logic to round 30+ seconds up a minute
       */
-      if (
-        !summaryStats ||
-        (!allowZeroHours && summaryStats.totalVolunteerHours <= 0.01)
-      )
-        continue
+    if (
+      !summaryStats ||
+      (!allowZeroHours && summaryStats.totalVolunteerHours <= 0.01)
+    )
+      return
 
-      await MailService.sendHourSummaryEmail(
-        firstName,
-        email,
-        sentHourSummaryIntroEmail,
-        lastMonday.format('dddd, MMM D'),
-        lastSunday.format('dddd, MMM D'),
-        summaryStats.totalCoachingHours,
-        summaryStats.totalElapsedAvailability,
-        summaryStats.totalQuizzesPassed,
-        summaryStats.totalVolunteerHours,
-        customCheck
-      )
-      if (!sentHourSummaryIntroEmail)
-        await updateVolunteerHourSummaryIntroById(volunteer.id)
-      totalEmailed++
-    } catch (error) {
-      errors.push(`${id}: ${error}\n`)
-    }
-  }
-
-  newrelic.recordMetric(`Job/${Jobs.EmailWeeklyHourSummary}`, totalEmailed)
-  log(
-    `Successfully ${Jobs.EmailWeeklyHourSummary} for ${totalEmailed} volunteers`
-  )
-  if (errors.length) {
+    await MailService.sendHourSummaryEmail(
+      firstName,
+      email,
+      sentHourSummaryIntroEmail,
+      start.format('dddd, MMM D'),
+      end.format('dddd, MMM D'),
+      summaryStats.totalCoachingHours,
+      summaryStats.totalElapsedAvailability,
+      summaryStats.totalQuizzesPassed,
+      summaryStats.totalVolunteerHours,
+      customCheck
+    )
+    if (!sentHourSummaryIntroEmail)
+      await updateVolunteerHourSummaryIntroById(id)
+  } catch (e) {
     throw new Error(
-      `Failed to ${Jobs.EmailWeeklyHourSummary} for volunteers:\n${errors}`
+      `Job/${Jobs.EmailWeeklyHourSummary} for userId: ${volunteer.id}, startDate: ${startDate}, endDate: ${endDate} with error: ${e}`
     )
   }
 }
---
+++ server/worker/jobs/spawnEmailWeeklyHourSummaryJobs.ts
@@ -0,0 +1,61 @@
+import moment from 'moment'
+import 'moment-timezone'
+import { Jobs } from '.'
+import { getVolunteersForWeeklyHourSummary } from '../../models/Volunteer/queries'
+import logger from '../../logger'
+import QueueService from '../../services/QueueService'
+
+// Runs weekly at 6am EST on Monday
+export default async (): Promise<void> => {
+  //  Monday-Sunday
+  const lastMonday = moment()
+    .utc()
+    .subtract(1, 'weeks')
+    .startOf('isoWeek')
+    .toISOString()
+  const lastSunday = moment()
+    .utc()
+    .subtract(1, 'weeks')
+    .endOf('isoWeek')
+    .toISOString()
+
+  const volunteers = await getVolunteersForWeeklyHourSummary()
+  const errors: { userId: string; error: unknown }[] = []
+  for (const volunteer of volunteers) {
+    try {
+      await QueueService.add(
+        Jobs.EmailWeeklyHourSummary,
+        {
+          startDate: lastMonday,
+          endDate: lastSunday,
+          volunteer,
+        },
+        {
+          /*
+            By default, all jobs have the highest priority of 1.
+            Since this job can spawn a few thousand jobs that aren't time sensitive,
+            we're setting priority to 3. That way, if we have 10,000 of these jobs
+            in the queue and a `NotifyTutors` job comes in, it can skip to the front
+            of the queue.
+          */
+          priority: 3,
+        }
+      )
+    } catch (error) {
+      errors.push({ userId: volunteer.id, error })
+    }
+  }
+
+  if (errors.length) {
+    logger.error(
+      '%s: Failed to queue %d! jobs: %o',
+      Jobs.SpawnEmailWeeklyHourSummaryJobs,
+      errors.length,
+      {
+        lastMonday,
+        lastSunday,
+        errors: errors,
+      }
+    )
+  }
+}
