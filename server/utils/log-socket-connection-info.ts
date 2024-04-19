c4d3cfa443b17285351cf31facb4c2f98846b7d4
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-04-19 15:48:22 +0000

    Log socket connection-related events with extra context

---
+++ server/utils/log-socket-connection-info.ts
@@ -0,0 +1,93 @@
+import { SocketUser } from '../router/extract-user'
+import { Ulid } from '../models/pgUtils'
+import logger from '../logger'
+
+// Taken from https://socket.io/docs/v4/server-socket-instance/#disconnect
+const DISCONNECT_REASONS = {
+  'server namespace disconnect': {
+    isError: false,
+    description:
+      'The socket was forcefully disconnected with socket.disconnect()',
+  },
+  'client namespace disconnect': {
+    isError: false,
+    description:
+      'The client has manually disconnected the socket using socket.disconnect()',
+  },
+  'server shutting down': {
+    isError: false,
+    description: 'The server is shutting down',
+  },
+  'ping timeout': {
+    isError: false,
+    description:
+      'The client did not send a PONG packet in the pingTimeout delay',
+  },
+  'transport close': {
+    isError: false,
+    description:
+      'The connection was closed (example: the user has lost connection, or the network was changed from WiFi to 4G)',
+  },
+  'transport error': {
+    isError: true,
+    description: 'The connection has encountered an error',
+  },
+  'parse error': {
+    isError: true,
+    description: 'The server has received an invalid packet from the client.',
+  },
+  'forced close': {
+    isError: true,
+    description: 'The server has received an invalid packet from the client.',
+  },
+  'forced server close': {
+    isError: false,
+    description:
+      'The client did not join a namespace in time (see the connectTimeout option) and was forcefully closed.',
+  },
+}
+
+export const connectionEvents = [
+  'connect',
+  'disconnect',
+  'client_reconnect',
+  'client_reconnect_attempt',
+  'client_connect_error',
+  'client_reconnect_error',
+  'leave',
+  'join',
+]
+
+export const logSocketConnectionInfo = (
+  event: string,
+  socket: SocketUser,
+  args?: any
+) => {
+  const userId = socket.request.user?.id as Ulid
+  const disconnectReason =
+    event === 'disconnect' || event === 'disconnection'
+      ? DISCONNECT_REASONS[args as keyof typeof DISCONNECT_REASONS]
+      : undefined
+
+  try {
+    const analyticsData = {
+      eventName: event,
+      disconnectReason: disconnectReason?.description,
+      disconnectIsError: disconnectReason?.isError,
+      user: {
+        id: userId,
+        isVolunteer: socket.request.user?.isVolunteer,
+      },
+      rooms: Array.from(socket.rooms),
+    }
+    const message = `Socket connection event: ${event}`
+    disconnectReason?.isError
+      ? logger.error(analyticsData, message)
+      : logger.info(analyticsData, message)
+  } catch (err) {
+    logger.error(
+      err,
+      `Failed to log socket connection info for userId=${userId}`
+    )
+  }
+}
