9af6b45d7b4fb0bc01f729f073ea471ad7757087
Liseidy Bueno <liseidy.bueno@upchieve.org>
2024-07-08 19:19:38 +0000

    remove unused functions from user controller

--- server/controllers/UserCtrl.ts
+++ server/controllers/UserCtrl.ts
@@ -3,14 +3,10 @@ import { captureException } from '@sentry/node'
 import * as USMRepo from '../models/UserSessionMetrics'
 import * as UPFRepo from '../models/UserProductFlags'
 import * as UserRepo from '../models/User'
-import * as StudentRepo from '../models/Student'
 import * as VolunteerRepo from '../models/Volunteer'
 import * as UserActionRepo from '../models/UserAction'
-import * as FederatedCredentialRepo from '../models/FederatedCredential'
-import { Certifications } from '../models/Volunteer'
 import { createContact } from '../services/MailService'
 import { hashPassword } from '../utils/auth-utils'
-import { emitter } from '../services/EventsService'
 import { logError } from '../logger'
 import { ACCOUNT_USER_ACTIONS, STUDENT_EVENTS } from '../constants'
 
@@ -26,75 +22,6 @@ export async function checkReferral(
       logError(error as Error)
     }
   }
-}
-
-export async function createStudentWithPassword(
-  studentData: StudentRepo.CreateStudentWithPasswordPayload,
-  ip: string
-) {
-  studentData.password = await hashPassword(studentData.password)
-  return createStudent(studentData, ip)
-}
-
-export async function createStudentWithFederatedCredential(
-  studentData: StudentRepo.CreateStudentWithFedCredPayload,
-  profileId: string,
-  issuer: string,
-  ip?: string
-) {
-  const student = await createStudent(studentData, ip)
-  await FederatedCredentialRepo.insertFederatedCredential(
-    profileId,
-    issuer,
-    student.id
-  )
-  return student
-}
-
-// TODO: duck type validation - studentData payload
-async function createStudent(
-  studentData: StudentRepo.CreateStudentPayload,
-  ip?: string
-): Promise<StudentRepo.CreatedStudent> {
-  const student = await StudentRepo.createStudent(studentData)
-
-  // Create a USM object for this new user
-  try {
-    await USMRepo.createUSMByUserId(student.id)
-  } catch (err) {
-    captureException(err)
-    logError(err as Error)
-  }
-
-  // Create a UPF object for this new user
-  try {
-    await UPFRepo.createUPFByUserId(student.id)
-  } catch (err) {
-    captureException(err)
-    logError(err as Error)
-  }
-
-  try {
-    await UserActionRepo.createAccountAction({
-      action: ACCOUNT_USER_ACTIONS.CREATED,
-      userId: student.id,
-      ipAddress: ip,
-    })
-  } catch (err) {
-    captureException(err)
-    logError(err as Error)
-  }
-
-  try {
-    await createContact(student.id)
-  } catch (err) {
-    captureException(err)
-    logError(err as Error)
-  }
-
-  emitter.emit(STUDENT_EVENTS.STUDENT_CREATED, student.id)
-
-  return student
 }
 
 // TODO: duck type validation - volunteerData payload
@@ -143,21 +70,4 @@ export async function createVolunteer(
 
   // needs to return id and partner org for frontend
   return volunteer
-}
-
-// TODO: I think we can nuke this pending reportutils finalization
-export function isCertified(certifications: Certifications): boolean {
-  let isCertified = false
-
-  for (const subject in certifications) {
-    if (
-      Object.prototype.hasOwnProperty.call(certifications, subject) &&
-      certifications[subject as keyof Certifications].passed
-    ) {
-      isCertified = true
-      break
-    }
-  }
-
-  return isCertified
 }
