8ca5f785318eb883b07e24cfa3c981bed444f4da
Alex L <alex.lindsay@upchieve.org>
2024-07-11 18:26:32 +0000

    Add topic_id column to teacher_classes yay!

--- database/db_init/test_seeds.sql
+++ database/db_init/test_seeds.sql
@@ -2840,8 +2840,8 @@ COPY upchieve.teacher_profiles (user_id, school_id, created_at, updated_at) FROM
 -- Data for Name: teacher_classes; Type: TABLE DATA; Schema: upchieve; Owner: admin
 --
 
-COPY upchieve.teacher_classes (id, user_id, name, code, active, created_at, updated_at) FROM stdin;
-01859800-bca8-af9e-8f1d-815bf6891cf5	01859800-bca8-af9e-8f1d-815bf6891cf5	some class	123123	t	2024-07-05 20:24:33.10427	2024-07-05 20:24:33.10427
+COPY upchieve.teacher_classes (id, user_id, name, code, active, created_at, updated_at, topic_id) FROM stdin;
+01859800-bca8-af9e-8f1d-815bf6891cf5	01859800-bca8-af9e-8f1d-815bf6891cf5	some class	123123	t	2024-07-05 20:24:33.10427	2024-07-05 20:24:33.10427	\N
 \.
 
 
