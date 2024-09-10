ac7f3d31f4898b50c97d2deb65d81675413b3a6c
Liseidy Bueno <liseidy.bueno@upchieve.org>
2024-09-10 20:12:53 +0000

    add student assignment sql query

--- server/models/Assignments/assignments.sql
+++ server/models/Assignments/assignments.sql
@@ -22,3 +22,30 @@ FROM
 WHERE
     id = :assignmentId!;
 
+
+/* @name createStudentAssignment */
+INSERT INTO students_assignments (user_id, assignment_id, created_at, updated_at)
+    VALUES (:userId!, :assignmentId!, NOW(), NOW())
+RETURNING
+    user_id, assignment_id, created_at, updated_at;
+
+
+/* @name getAssignmentsByStudentId */
+SELECT
+    assignments.class_id,
+    assignments.description,
+    assignments.title,
+    assignments.number_of_sessions,
+    assignments.min_duration_in_minutes,
+    assignments.due_date,
+    assignments.subject_id,
+    assignments.start_date,
+    assignments.is_required,
+    assignments.id,
+    students_assignments.submitted_at
+FROM
+    assignments
+    LEFT JOIN students_assignments ON assignments.id = students_assignments.assignment_id
+WHERE
+    students_assignments.user_id = :userId!;
+
