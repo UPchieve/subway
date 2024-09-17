86a117c3f43d390826fc4febbc99692e30ffe755
Liseidy Bueno <liseidy.bueno@upchieve.org>
2024-09-17 15:31:53 +0000

    Add get student by assignment id to router

--- server/router/api/assignments.ts
+++ server/router/api/assignments.ts
@@ -3,13 +3,25 @@ import * as AssignmentsService from '../../services/AssignmentsService'
 import { resError } from '../res-error'
 
 export function routeAssignments(router: Router): void {
-  router.route('/assignment/:assignmentId').get(async function(req, res) {
+  router.get('/assignment/:assignmentId', async function(req, res) {
     try {
       const assignmentId = req.params.assignmentId as string
       const assignment = await AssignmentsService.getAssignmentById(
         assignmentId
       )
       res.json({ assignment })
+    } catch (err) {
+      resError(res, err)
+    }
+  })
+
+  router.get('/assignment/:assignmentId/students', async function(req, res) {
+    try {
+      const assignmentId = req.params.assignemntId as string
+      const studentIds = await AssignmentsService.getStudentsByAssignmentId(
+        assignmentId
+      )
+      res.json({ studentIds })
     } catch (err) {
       resError(res, err)
     }
