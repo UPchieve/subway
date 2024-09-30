8145157bfe50428d0145e4c5ab88a27fa5454689
Jamie Kasulis <jamie.kasulis@upchieve.org>
2024-09-30 19:04:08 +0000

    Revert "Tutor bot conversations unique constraint"

--- database/migrations/20240930173555_tutor-bot-conversations-unique-on-session-id.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-ALTER TABLE IF EXISTS upchieve.tutor_bot_conversations
-    ADD CONSTRAINT session_id_unique UNIQUE (session_id);
-
--- migrate:down
-ALTER TABLE IF EXISTS upchieve.tutor_bot_conversations
-    DROP CONSTRAINT IF EXISTS session_id_unique;
-
