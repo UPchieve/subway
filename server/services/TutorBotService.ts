50894c8d4a0d3b9474f0c9f7a599f4a79eee97f4
sammy nave <sammy.nave@gmail.com>
2024-10-17 16:47:17 +0000

    Manage prompts via Langfuse and keep only last 15 messages in context

--- server/services/TutorBotService.ts
+++ server/services/TutorBotService.ts
@@ -19,6 +19,8 @@ import SocketService from './SocketService'
 import { openai } from './BotsService'
 import * as FeatureFlagService from './FeatureFlagService'
 import { getSubjectNameIdMapping } from '../models/Subjects'
+import { TextPromptClient } from 'langfuse-core'
+import { COLLEGE_SUBJECTS } from '../constants'
 
 const LF_TRACE_NAME = 'tutorBotSession'
 const LF_GENERATION_NAME = 'tutorBotSessionMessage'
@@ -231,50 +233,52 @@ export const addMessageToConversation = async (
   }, parentTransaction)
 }
 
-const openAiPrompt = (subject: string, conversation: string) => `
-You are an experienced ${subject} teacher. Your task is to participate in a tutoring session with a student and possibly a volunteer tutor. A conversation snippet
-will be provided for you, each message will start with an identifier (<|student|>, <|volunteer|>, or <|bot|> (you are the bot)) and end with '<|end|>'.
-You should then determine what strategy you want to use to assist the student in learning the subject and completing the problem or answering their questions.
-State your intention in using that strategy. We have a list of common strategies and intentions that teachers use, which you can pick from.
-We also give you the option to write in your own own strategy or intention if none of the options apply.
+async function getPromptDataFor(
+  subjectName: string
+): Promise<{
+  isFallback: boolean
+  prompt: string
+  version: string
+  promptObject?: TextPromptClient
+}> {
+  const promptName = Object.values<string>(COLLEGE_SUBJECTS).includes(
+    subjectName
+  )
+    ? LangfuseService.LangfusePromptNameEnum.TUTOR_BOT_COLLEGE_COUNSELING_PROMPT
+    : LangfuseService.LangfusePromptNameEnum.TUTOR_BOT_GENERIC_SUBJECT_PROMPT
+  const promptFromLangfuse = await LangfuseService.getPrompt(promptName)
+  const isFallback = promptFromLangfuse === undefined
 
-Strategies:
-0. Explain a concept
-1. Ask a question
-2. Provide a hint
-3. Provide a strategy
-4. Provide a worked example
-5. Provide a minor correction
-6. Provide a similar problem
-7. Simplify the question
-8. Affirm the correct answer
-9. Encourage the student
-10. Other (please specify in your reasoning)
+  const prompt = isFallback
+    ? TUTOR_BOT_GENERIC_SUBJECT_PROMPT_FALLBACK
+    : (promptFromLangfuse! as TextPromptClient).prompt
 
-Intentions:
-0. Motivate the student
-1. Get the student to elaborate their answer
-2. Correct the student's mistake
-3. Hint at the student's mistake
-4. Clarify a student's misunderstanding
-5. Help the student understand the lesson topic or solution strategy
-6. Diagnose the student's mistake
-7. Support the student in their thinking or problem-solving
-8. Explain the student's mistake (eg. what is wrong in their answer or why is it incorrect)
-9. Signal to the student that they have solved or not solved the problem
-10. Other (please specify in your reasoning)
+  return {
+    isFallback,
+    prompt,
+    version: isFallback
+      ? 'FALLBACK'
+      : `${promptFromLangfuse!.name}-${promptFromLangfuse!.version}`,
+    ...(!isFallback && {
+      promptObject: promptFromLangfuse as TextPromptClient,
+    }),
+  }
+}
 
-Here is the conversation snippet:
-Lesson topic: ${subject}
-Conversation:
-${conversation}<|end|>
+function interpolate({
+  text,
+  replacements,
+}: {
+  text: string
+  replacements: Record<string, string>
+}) {
+  return Object.entries(replacements).reduce((acc, [target, value]) => {
+    const reg = new RegExp(target, 'g')
+    return acc.replace(reg, value)
+  }, text)
+}
 
-How would you help the student understand and solve the problem and why? Pick the option number from the list of strategies and intentions and provide the reason behind your choices.
-Then, using your choices, respond to the student as an experienced math teacher and helpful tutor. Do not give them a direct answer but use your strategy and intentions to craft a concise, useful, and caring response
-to help the student with the next step in solving the given problem or better understanding the subject.
-Format your answer as a JSON object: {"strategy": #, "intention": #, "reason": "write out your reason for picking that strategy and intention", "response": "write out your response to the student's last message"}
-`
-
+const NUM_OF_MESSAGES_TO_KEEP_IN_CONTEXT = 15
 const getOpenAiBotResponse = async (
   {
     userId,
@@ -296,17 +300,24 @@ const getOpenAiBotResponse = async (
     name: LF_TRACE_NAME,
     sessionId: conversationId,
   })
+  // NOTE these are ordered by created at ASC
   const transcript = await getTranscriptForConversation(conversationId, tc)
-  const prompt = transcript.messages
+  const mostRecentMessages = transcript.messages
     .map(({ senderUserType, message }) => `<|${senderUserType}|>: ${message}`)
+    .slice(-NUM_OF_MESSAGES_TO_KEEP_IN_CONTEXT)
     .join('<|end|>\n')
-
   const gen = t.generation({
     name: LF_GENERATION_NAME,
     model: TUTOR_BOT_MODELS.CHAT_GPT_4O,
   })
-
-  const messagePrompt = openAiPrompt(subjectName, prompt)
+  const promptData = await getPromptDataFor(subjectName)
+  const messagePrompt = interpolate({
+    text: promptData.prompt,
+    replacements: {
+      '{{subject}}': subjectName,
+      '{{conversation}}': mostRecentMessages,
+    },
+  })
 
   const completion = await openai.chat.completions.create({
     model: TUTOR_BOT_MODELS.CHAT_GPT_4O,
@@ -339,7 +350,7 @@ const getOpenAiBotResponse = async (
   )
   gen.end({
     output: content.response,
-    input: { prompt, ...savedBotMessage, subjectName },
+    input: { prompt: messagePrompt, ...savedBotMessage, subjectName },
   })
 
   return {
@@ -492,3 +503,54 @@ const extractSystem = (text: string): { assistant: string; system: string } => {
 const removeTurnMarkers = (text: string): string => {
   return text.replace(/<\|user\|>|<\|assistant\|>|<\|end\|>/g, '').trim()
 }
+
+/* NOTE
+ *
+ * The prompt MUST contain the word 'json' in some form
+ * and MUST return a json object with the bot's message in the key 'response'
+ *
+ * e.g. `Format your answer as a JSON object: {"response": "write out your response to the student's last message"}`
+ */
+const TUTOR_BOT_GENERIC_SUBJECT_PROMPT_FALLBACK = `
+You are an experienced {{subject}} teacher. Your task is to participate in a tutoring session with a student and possibly a volunteer tutor. A conversation snippet
+will be provided for you, each message will start with an identifier (<|student|>, <|volunteer|>, or <|bot|> (you are the bot)) and end with '<|end|>'.
+You should then determine what strategy you want to use to assist the student in learning the subject and completing the problem or answering their questions.
+State your intention in using that strategy. We have a list of common strategies and intentions that teachers use, which you can pick from.
+We also give you the option to write in your own own strategy or intention if none of the options apply.
+
+Strategies:
+0. Explain a concept
+1. Ask a question
+2. Provide a hint
+3. Provide a strategy
+4. Provide a worked example
+5. Provide a minor correction
+6. Provide a similar problem
+7. Simplify the question
+8. Affirm the correct answer
+9. Encourage the student
+10. Other (please specify in your reasoning)
+
+Intentions:
+0. Motivate the student
+1. Get the student to elaborate their answer
+2. Correct the student's mistake
+3. Hint at the student's mistake
+4. Clarify a student's misunderstanding
+5. Help the student understand the lesson topic or solution strategy
+6. Diagnose the student's mistake
+7. Support the student in their thinking or problem-solving
+8. Explain the student's mistake (eg. what is wrong in their answer or why is it incorrect)
+9. Signal to the student that they have solved or not solved the problem
+10. Other (please specify in your reasoning)
+
+Here is the conversation snippet:
+Lesson topic: {{subject}}
+Conversation:
+{{conversation}}<|end|>
+
+How would you help the student understand and solve the problem and why? Pick the option number from the list of strategies and intentions and provide the reason behind your choices.
+Then, using your choices, respond to the student as an experienced math teacher and helpful tutor. Do not give them a direct answer but use your strategy and intentions to craft a concise, useful, and caring response
+to help the student with the next step in solving the given problem or better understanding the subject.
+Format your answer as a JSON object: {"strategy": #, "intention": #, "reason": "write out your reason for picking that strategy and intention", "response": "write out your response to the student's last message"}
+`
