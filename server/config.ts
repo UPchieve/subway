5cd685820b61be0259dafe1665742a93168024dd
Jamie Kasulis <jamie.kasulis@upchieve.org>
2025-01-23 12:56:37 -0500

    update config

--- server/config.ts
+++ server/config.ts
@@ -22,7 +22,7 @@ const config: Static<typeof Config> = {
   NODE_ENV: process.env.NODE_ENV || 'dev',
   SSL_CERT_PATH: '',
   // set host to your public IP address to test Twilio voice calling
-  host: process.env.SUBWAY_SERVER_HOST || 'localhost:3000',
+  host: process.env.SUBWAY_SERVER_HOST || 'http://localhost:8080',
   additionalAllowedOrigins: process.env.SUBWAY_ADDITIONAL_ALLOWED_ORIGINS || '',
   sessionSecret: process.env.SUBWAY_SESSION_SECRET || 'secret',
   sessionCookieMaxAge: parseInt(
