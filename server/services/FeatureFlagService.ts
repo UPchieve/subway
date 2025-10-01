775d1b54e5dd1a5eb2456c8963a85103cf1f793c
Trey Stevens <treykstevens.tks@gmail.com>
2025-10-01 18:28:36 +0000

    Fix session summary flags

--- server/services/FeatureFlagService.ts
+++ server/services/FeatureFlagService.ts
@@ -151,13 +151,11 @@ export async function getTeacherGettingStartedAssignmentFlag(userId: Uuid) {
 }
 
 export async function getGenerateSessionSummaryFeatureFlag(userId: Uuid) {
-  return true
-  // return await isFeatureEnabled(FEATURE_FLAGS.GENERATE_SESSION_SUMMARY, userId)
+  return await isFeatureEnabled(FEATURE_FLAGS.GENERATE_SESSION_SUMMARY, userId)
 }
 
 export async function getSessionSummaryFeatureFlag(userId: Uuid) {
-  return true
-  // return await isFeatureEnabled(FEATURE_FLAGS.GET_SESSION_SUMMARY, userId)
+  return await isFeatureEnabled(FEATURE_FLAGS.GET_SESSION_SUMMARY, userId)
 }
 
 export async function getDisplayVolunteerLanguagesFlag(userId: Uuid) {
