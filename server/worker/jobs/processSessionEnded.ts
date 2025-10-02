f42bac21f4ace8e26929dec922d21a35a98c99f4
Kasozi Wilson <kasozi.wilson@upchieve.org>
2025-10-02 11:05:55 -0400

    Investigate Procession Session Ended

--- server/worker/jobs/processSessionEnded.ts
+++ server/worker/jobs/processSessionEnded.ts
@@ -14,10 +14,21 @@ import {
 } from '../../services/SessionService'
 import { asString } from '../../utils/type-utils'
 import { queueGenerateSessionSummaryForSession } from '../../services/SessionSummariesService'
+import logger from '../../logger'
 
 type ProcessSessionEndedJobData = {
   sessionId: Uuid
 }
+
+const METRIC_TASKS = [
+  queueGenerateSessionSummaryForSession,
+  processEmailVolunteer,
+  processFirstSessionCongratsEmail,
+  queueFallIncentiveSessionQualificationJob,
+  queueGenerateProgressReportForUser,
+]
+
+const AFTER_METRIC_TASK = [processSessionReported, processSessionTranscript]
 
 export default async (job: Job<ProcessSessionEndedJobData>): Promise<void> => {
   const sessionId = asString(job.data.sessionId)
@@ -28,19 +39,19 @@ export default async (job: Job<ProcessSessionEndedJobData>): Promise<void> => {
     await processSessionEditors(sessionId)
 
     // Dependent on metrics being calculated first
-    await Promise.all([
-      queueGenerateSessionSummaryForSession(sessionId),
-      processEmailVolunteer(sessionId),
-      processFirstSessionCongratsEmail(sessionId),
-      queueFallIncentiveSessionQualificationJob(sessionId),
-      queueGenerateProgressReportForUser(sessionId),
-    ])
+    for (const task of METRIC_TASKS) {
+      logger.info(`Starting metric task ${task.name}`)
+      await task(sessionId)
+      logger.info(`Finished metric task ${task.name}`)
+    }
 
-    await Promise.all([
-      processSessionReported(sessionId),
-      processSessionTranscript(sessionId),
-    ])
+    for (const afterMetricTask of AFTER_METRIC_TASK) {
+      logger.info(`Starting after metric task ${afterMetricTask.name}`)
+      await afterMetricTask(sessionId)
+      logger.info(`Finished after metric task ${afterMetricTask.name}`)
+    }
   } catch (error) {
+    logger.error(error)
     throw new Error(
       `Failed to complete ${Jobs.ProcessSessionEnded} for session ${sessionId}: ${error}`
     )
