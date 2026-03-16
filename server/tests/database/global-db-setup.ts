24fe8a345a7c937c5f0654e46ac1eb86c4b5dcda
Alex Lindsay <alex.lindsay@upchieve.org>
2026-03-16 12:31:40 -0700

    Add `--wait` when starting up local test database.

--- server/tests/database/global-db-setup.ts
+++ server/tests/database/global-db-setup.ts
@@ -6,7 +6,7 @@ export default async function globalSetup(): Promise<void> {
   if (!isCI) {
     try {
       execSync('docker compose --profile db-test down')
-      execSync('docker compose --profile db-test up -d')
+      execSync('docker compose --profile db-test up -d --wait')
     } catch (error) {
       console.error('Error in global setup:', (error as Error).message)
       process.exit(1)
