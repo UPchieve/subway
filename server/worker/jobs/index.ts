53d1175e2fac5dcd7214e5d1ef8abf3083167b57
Alex Lindsay <alex.lindsay@upchieve.org>
2026-05-13 21:50:38 +0000

    Delete backfill-referrals-table script.

--- server/worker/jobs/index.ts
+++ server/worker/jobs/index.ts
@@ -6,7 +6,6 @@ import backfillAvailabilityHistories from '../../scripts/backfill-availability-h
 import backfillElapsedAvailability from '../../scripts/backfill-elapsed-availability'
 import backfillEmailNiceToMeetYou from '../../scripts/backfill-email-nice-to-meet-you'
 import backfillEmailVolunteerInactive from '../../scripts/backfill-email-volunteer-inactive'
-import backfillReferralsTable from '../../scripts/backfill-referrals-table'
 import backfillStudentPosthog from '../../scripts/backfill-student-posthog'
 import backfillStudentUsersRoles from '../../scripts/backfill-student-users-roles'
 import backfillUsersGradeLevels from '../../scripts/backfill-users-grade-levels'
@@ -82,7 +81,6 @@ export enum Jobs {
   BackfillElapsedAvailability = 'BackfillElapsedAvailability',
   BackfillEmailNiceToMeetYou = 'BackfillEmailNiceToMeetYou',
   BackfillEmailVolunteersInactive = 'BackfillEmailVolunteersInactive',
-  BackfillReferralsTable = 'BackfillReferralsTable',
   BackfillSessionEndedTasks = 'BackfillSessionEndedTasks',
   BackfillStudentAmbassadorRole = 'BackfillStudentAmbassadorRole',
   BackfillStudentPosthog = 'BackfillStudentPosthog',
@@ -193,10 +191,6 @@ const jobProcessors: JobProcessor[] = [
   {
     name: Jobs.BackfillEmailVolunteersInactive,
     processor: backfillEmailVolunteerInactive,
-  },
-  {
-    name: Jobs.BackfillReferralsTable,
-    processor: backfillReferralsTable,
   },
   {
     name: Jobs.BackfillSessionEndedTasks,
